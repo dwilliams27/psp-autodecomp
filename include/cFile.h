f51e0647da84ad5b63d5101c14fbd6014a373f7a
autodecomp <autodecomp@localhost>
2026-04-07 22:51:59 -0500

    Match 5 functions (session ea243e35)
      - cFile::OnCreated
      - cFile::OnClosed
      - cListSubscriber::SetNumVisible
      - cLanguage::PlatformInitialize
      - cBufferedFile::GetSize

---
+++ include/cFile.h
@@ -0,0 +1,31 @@
+#ifndef CFILE_H
+#define CFILE_H
+
+class cFile {
+public:
+    void OnCreated(void);
+    void OnClosed(void);
+};
+
+class cListSubscriber {
+public:
+    char pad[0x20];
+    int mNumVisible;
+
+    void SetNumVisible(int numVisible);
+};
+
+class cLanguage {
+public:
+    static int PlatformInitialize(void);
+};
+
+class cBufferedFile {
+public:
+    char pad[0x128];
+    int mSize;
+
+    int GetSize(void) const;
+};
+
+#endif
---
+++ src/cFile.cpp
@@ -0,0 +1,19 @@
+#include "cFile.h"
+
+void cFile::OnCreated(void) {
+}
+
+void cFile::OnClosed(void) {
+}
+
+void cListSubscriber::SetNumVisible(int numVisible) {
+    mNumVisible = numVisible;
+}
+
+int cLanguage::PlatformInitialize(void) {
+    return 1;
+}
+
+int cBufferedFile::GetSize(void) const {
+    return mSize;
+}
