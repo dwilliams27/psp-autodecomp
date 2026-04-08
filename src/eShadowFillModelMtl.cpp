012e2cc0ffed30cf7eac62fc76c5299769503016
autodecomp <autodecomp@localhost>
2026-04-08 00:27:55 -0500

    Match 5 functions (session deb43e29)
      - eShadowFillModelMtl::CreateData
      - eBumpOffsetMap::PlatformFree
      - eBumpOffsetMap::CreateData
      - eBumpOffsetMap::Apply
      - eDynamicLightHeightmapMtl::PlatformFree

---
+++ src/eShadowFillModelMtl.cpp
@@ -0,0 +1,16 @@
+#include "eTextureMap.h"
+
+void eShadowFillModelMtl::CreateData(void) {
+}
+
+void eBumpOffsetMap::PlatformFree(void) {
+}
+
+void eBumpOffsetMap::CreateData(void) {
+}
+
+void eBumpOffsetMap::Apply(int, const eCamera *, const eWorld *) const {
+}
+
+void eDynamicLightHeightmapMtl::PlatformFree(void) {
+}
