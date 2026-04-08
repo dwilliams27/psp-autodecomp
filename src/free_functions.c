176ab934bd298c84d7a77ef6ea25d77459e8e4b9
autodecomp <autodecomp@localhost>
2026-04-08 01:49:43 -0500

    Match 5 functions (session e495b330)
      - _fini
      - cBase::SetDirty
      - cBase::IsEditable
      - cBase::Reset
      - cBase::GetName

--- src/free_functions.c
+++ src/free_functions.c
@@ -15,3 +15,20 @@ int cFileSystem_Create(const char *name) {
 int cFileSystem_Write(void *handle, const void *data, unsigned int size) {
     return 0;
 }
+
+void _fini(void) {
+}
+
+void cBase_SetDirty(void *self) {
+}
+
+int cBase_IsEditable(const void *self) {
+    return 0;
+}
+
+void cBase_Reset(void *self, void *pool, int reset) {
+}
+
+void cBase_GetName(const void *self, char *name) {
+    *name = 0;
+}
