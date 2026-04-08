c97af07787c04cc1ad78a56da0f6849a04282aaa
autodecomp <autodecomp@localhost>
2026-04-08 02:02:42 -0500

    Match 5 functions (session a0f063c8)
      - gcExpression::GetTextColor
      - gcExpression::GetNext
      - gcExpression::SetNext
      - gcExpression::GetExprFlags
      - gcAction::GetExprFlags

--- src/gcExpression.cpp
+++ src/gcExpression.cpp
@@ -17,3 +17,22 @@ gcExpression *gcExpression::GetBranch(int) const {
 
 void gcExpression::SetBranch(int, gcExpression *) {
 }
+
+unsigned int gcExpression::GetTextColor(void) const {
+    return 0xFF000000;
+}
+
+gcExpression *gcExpression::GetNext(void) const {
+    return 0;
+}
+
+void gcExpression::SetNext(gcExpression *) {
+}
+
+int gcExpression::GetExprFlags(void) const {
+    return 0;
+}
+
+int gcAction::GetExprFlags(void) const {
+    return 0xB;
+}
