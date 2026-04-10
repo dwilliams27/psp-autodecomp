27bc83e807af86f1ce9098eb46f083d224161990
autodecomp <autodecomp@localhost>
2026-04-10 00:25:48 -0500

    Match 5 functions (session c1f43230)
      - gcUIDialog::OnFinalOpen
      - gcUIDialog::ClearEvents
      - gcUIDialog::Reset
      - gcUIDialog::Focus
      - gcUIDialog::Close

--- include/gcUIDialog.h
+++ include/gcUIDialog.h
@@ -1,9 +1,30 @@
 #ifndef GCUIDIALOG_H
 #define GCUIDIALOG_H
 
+class gcUI;
+class cMemPool;
+class gcUIWidgetGroup;
+class gcDoUIShowDialog;
+
 class gcUIDialog {
 public:
+    char pad_000[0x44];
+    gcUI *mpUI;                    // 0x44
+    char pad_048[0x08];
+    int mFlags;                    // 0x50
+    char pad_054[0x1E4];
+    int mEvent0;                   // 0x238
+    int mEvent1;                   // 0x23C
+    int mEvent2;                   // 0x240
+    int mEvent3;                   // 0x244
+    int mEvent4;                   // 0x248
+
     void CalcInstanceSize(void);
+    void OnFinalOpen(void);
+    void ClearEvents(void);
+    void Reset(cMemPool *pool, bool flag);
+    void Focus(void);
+    void Close(void);
 };
 
 #endif
--- src/gcUIDialog.cpp
+++ src/gcUIDialog.cpp
@@ -1,4 +1,34 @@
 #include "gcUIDialog.h"
+#include "gcUI.h"
+#include "gcUIWidgetGroup.h"
 
 void gcUIDialog::CalcInstanceSize(void) {
+}
+
+void gcUIDialog::OnFinalOpen(void) {
+    mFlags = mFlags & ~0x10;
+}
+
+void gcUIDialog::ClearEvents(void) {
+    mEvent0 = 0;
+    mEvent1 = 0;
+    mEvent2 = 0;
+    mEvent3 = 0;
+    mEvent4 = 0;
+}
+
+void gcUIDialog::Reset(cMemPool *pool, bool flag) {
+    ((gcUIWidgetGroup *)((char *)this + 0x98))->Reset(pool, true);
+}
+
+void gcUIDialog::Focus(void) {
+    if (mpUI) {
+        mpUI->ActualOpenDialog(this);
+    }
+}
+
+void gcUIDialog::Close(void) {
+    if (mpUI) {
+        mpUI->CloseDialog(this, 0, 0);
+    }
 }
