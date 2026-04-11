48fb96cc6cc4adacb1e15673dceabb746725f852
Farmerbilly27 <dmw2796@gmail.com>
2026-04-11 04:30:53 -0500

    Match 1 functions (session b1d1da2b)
      - eCollisionConstraint::Allocate

--- src/eCollisionConstraint.cpp
+++ src/eCollisionConstraint.cpp
@@ -1,7 +1,17 @@
-extern "C" {
+#include "eCollisionConstraint.h"
 
-void gcUIDialog__OnFinalOpen_void(void *self) {
-    *(int *)((char *)self + 0x50) &= ~0x10;
-}
+extern eCollisionConstraint *gCollisionConstraintFreeList;  // 0x37D320
 
+eCollisionConstraint *eCollisionConstraint::Allocate(const eContactFeature &feature) {
+    eCollisionConstraint *node = gCollisionConstraintFreeList;
+    if (node) {
+        gCollisionConstraintFreeList = node->mNext;
+        __asm__ volatile("" ::: "memory");
+        node->mValue = 0.0f;
+        eContactFeature *dst = &node->mFeature;
+        *dst = feature;
+        node->mFlags = 0;
+        return node;
+    }
+    return 0;
 }
