e6d4be3469be529129f4b10230518c7f040e2004
autodecomp <autodecomp@localhost>
2026-04-20 06:20:04 -0500

    Match 1 functions (session 8d286f30)
      - eSphereShape::GetSupport

--- include/eSphereShape.h
+++ include/eSphereShape.h
@@ -1,9 +1,22 @@
 #ifndef ESPHERESHAPE_H
 #define ESPHERESHAPE_H
 
+struct mVec3;
+struct mOCS;
+class eMultiSphereShape;
+class eCollisionContactInfo;
+
 class eSphereShape {
 public:
+    char _pad[0x74];
+    float mRadius;  // 0x74
+
     int CanSweep(void) const;
+    float GetVolume(void) const;
+    void GetSupport(const mVec3 &dir, const mOCS &ocs, mVec3 *out) const;
+    void GetProjectedMinMax(const mVec3 &dir, const mOCS &ocs, float *outMin, float *outMax) const;
+    void GetInertialTensor(float mass, mVec3 *out) const;
+    int Collide(const eMultiSphereShape *, int, int, const mOCS &, const mOCS &, eCollisionContactInfo *) const;
 };
 
 #endif
--- src/eSphereShape.cpp
+++ src/eSphereShape.cpp
@@ -1,5 +1,67 @@
 #include "eSphereShape.h"
+#include "eMultiSphereShape.h"
+#include "eCollision.h"
+#include "mVec3.h"
+#include "mOCS.h"
 
 int eSphereShape::CanSweep(void) const {
     return 1;
+}
+
+float eSphereShape::GetVolume(void) const {
+    float r = mRadius;
+    return r * r * r * 4.1887903f;
+}
+
+void eSphereShape::GetSupport(const mVec3 &dir, const mOCS &ocs, mVec3 *out) const {
+    float r = mRadius;
+    SceULong128 pos;
+    __asm__ volatile(
+        "mfc1 $a0, %1\n"
+        "mtv $a0, S100\n"
+        "lv.q %0, 0x30(%2)\n"
+        "lv.q C130, 0(%3)\n"
+        "vscl.t C130, C130, S100\n"
+        "vadd.t %0, %0, C130\n"
+        : "=X"(pos)
+        : "f"(r), "r"(&ocs), "r"(&dir)
+        : "$a0"
+    );
+    *(SceULong128 *)out = pos;
+}
+
+void eSphereShape::GetProjectedMinMax(const mVec3 &dir, const mOCS &ocs, float *outMin, float *outMax) const {
+    float dot;
+    __asm__ volatile(
+        "lv.q C120, 0x30(%1)\n"
+        "lv.q C130, 0(%2)\n"
+        "vdot.t S100, C120, C130\n"
+        "mfv $a1, S100\n"
+        "mtc1 $a1, %0\n"
+        : "=f"(dot)
+        : "r"(&ocs), "r"(&dir)
+        : "$a1"
+    );
+    *outMin = dot - mRadius;
+    *outMax = dot + mRadius;
+}
+
+void eSphereShape::GetInertialTensor(float mass, mVec3 *out) const {
+    float v = mass * 0.4f * (mRadius * mRadius);
+    int a, b, c;
+    __asm__ volatile("mfc1 %0, %1" : "=r"(a) : "f"(v));
+    __asm__ volatile("mfc1 %0, %1" : "=r"(b) : "f"(v));
+    __asm__ volatile("mfc1 %0, %1" : "=r"(c) : "f"(v));
+    __asm__ volatile(
+        "mtv %0, S120\n"
+        "mtv %1, S121\n"
+        "mtv %2, S122\n"
+        "sv.q C120, 0(%3)\n"
+        :: "r"(a), "r"(b), "r"(c), "r"(out)
+        : "memory"
+    );
+}
+
+int eSphereShape::Collide(const eMultiSphereShape *ms, int, int, const mOCS &ocs1, const mOCS &ocs2, eCollisionContactInfo *info) const {
+    return eCollision::SphereMultiSphere(*this, *ms, ocs1, ocs2, info);
 }
