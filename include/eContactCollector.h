ff1a67eb94a14b92f8db42c0aff53c089fbd963f
autodecomp <autodecomp@localhost>
2026-04-20 05:38:16 -0500

    Match 2 functions (session f83dbb76)
      - eContactCollector::ClearAllContacts
      - eContactCollector::ValidateTri

--- include/eContactCollector.h
+++ include/eContactCollector.h
@@ -1,11 +1,19 @@
 #ifndef ECONTACTCOLLECTOR_H
 #define ECONTACTCOLLECTOR_H
 
+class mVec3;
+class eContact;
+
 class eContactCollector {
 public:
     enum eContactType {};
 
     void ClearContacts(eContactType);
+    void ClearAllContacts(void);
+    void FindSlideVelocity(mVec3 *) const;
+    int ValidateTri(eContact *, int);
+    int ValidatePoint(eContact *, int, int);
+    void VerifySoftContacts(void);
 };
 
 #endif
