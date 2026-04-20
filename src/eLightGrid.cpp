c56707ed7d8cbf31818cbc1ce60e1ae0846b2ea2
autodecomp <autodecomp@localhost>
2026-04-20 04:24:32 -0500

    Match 1 functions (session 69211f64)
      - eLightGrid::eLightGrid

---
+++ src/eLightGrid.cpp
@@ -0,0 +1,125 @@
+#include "cBase.h"
+#include "cFile.h"
+
+inline void *operator new(unsigned int, void *p) { return p; }
+
+class cMemPool;
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int, const cType *, cBase *(*)(cMemPool *, cBase *), const char *, const char *, unsigned int);
+};
+
+class cReadBlock {
+public:
+    int _data[5];
+    cReadBlock(cFile &, unsigned int, bool);
+    ~cReadBlock(void);
+};
+
+class eLightGridNode;
+class eLightGridSample;
+
+template <class T>
+class cArrayBase {
+public:
+    void *_data;
+    cArrayBase &operator=(const cArrayBase &);
+};
+
+template <class T>
+class cArray : public cArrayBase<T> {
+public:
+    void Read(cReadBlock &);
+};
+
+class eLightGrid {
+public:
+    cBase *mOwner;
+    void *mClassDesc;
+    cArray<eLightGridNode> mNodes;
+    cArray<eLightGridSample> mSamples;
+
+    eLightGrid(cBase *);
+    int Read(cFile &, cMemPool *);
+    void AssignCopy(const cBase *);
+    static cBase *New(cMemPool *, cBase *);
+    const cType *GetType(void) const;
+};
+
+template <class T> T *dcast(const cBase *);
+
+extern char eLightGridclassdesc[];
+extern const char eLightGrid_base_name[];
+extern const char eLightGrid_base_desc[];
+
+static cType *type_base;
+static cType *type_eLightGrid;
+
+// --- ctor ---
+eLightGrid::eLightGrid(cBase *parent) {
+    mOwner = parent;
+    mClassDesc = eLightGridclassdesc;
+    mNodes._data = 0;
+    mSamples._data = 0;
+}
+
+void cFile_SetCurrentPos(void *, unsigned int);
+
+// --- Read ---
+int eLightGrid::Read(cFile &file, cMemPool *) {
+    int result = 1;
+    cReadBlock rb(file, 2, true);
+    if ((unsigned int)rb._data[3] != 2) {
+        cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+        return 0;
+    }
+    mNodes.Read(rb);
+    mSamples.Read(rb);
+    return result;
+}
+
+// --- AssignCopy ---
+void eLightGrid::AssignCopy(const cBase *src) {
+    eLightGrid &rhs = *dcast<eLightGrid>(src);
+    mNodes = rhs.mNodes;
+    mSamples = rhs.mSamples;
+}
+
+// --- New ---
+struct PoolBlock {
+    char pad[0x1C];
+    char *allocTable;
+};
+
+struct AllocEntry {
+    short offset;
+    short pad;
+    void *(*fn)(void *, int, int, int, int);
+};
+
+cBase *eLightGrid::New(cMemPool *pool, cBase *parent) {
+    void *block = ((void **)pool)[9];
+    char *allocTable = ((PoolBlock *)block)->allocTable;
+    AllocEntry *entry = (AllocEntry *)(allocTable + 0x28);
+    short off = entry->offset;
+    void *base = (char *)block + off;
+    eLightGrid *result = 0;
+    eLightGrid *obj = (eLightGrid *)entry->fn(base, 0x10, 4, 0, 0);
+    if (obj != 0) {
+        new (obj) eLightGrid(parent);
+        result = obj;
+    }
+    return (cBase *)result;
+}
+
+// --- GetType ---
+const cType *eLightGrid::GetType(void) const {
+    if (!type_eLightGrid) {
+        if (!type_base) {
+            type_base = cType::InitializeType(eLightGrid_base_name, eLightGrid_base_desc, 1, 0, 0, 0, 0, 0);
+        }
+        type_eLightGrid = cType::InitializeType(0, 0, 0x1BA, type_base, &eLightGrid::New, 0, 0, 0);
+    }
+    return type_eLightGrid;
+}
