734b4239d87467b5794b1c590ec1116b848838c3
autodecomp <autodecomp@localhost>
2026-04-21 17:52:21 -0500

    Match 5 functions (session 26f3a403)
      - gcStringValue::Write
      - gcTableColumn::Write
      - gcPartialEntityController::Write
      - gcSubGeomController::Write
      - gcDoSwitch::GetText

--- src/gcStringValue.cpp
+++ src/gcStringValue.cpp
@@ -1,3 +1,5 @@
+class cFile;
+
 class gcStringTable;
 class gcString;
 
@@ -20,13 +22,69 @@ public:
     U mSubHandle;
 };
 
+class cWriteBlock {
+public:
+    int _data[2];
+    cWriteBlock(cFile &, unsigned int);
+    void End(void);
+};
+
+extern "C" {
+    void cStrCat(char *, const char *);
+}
+
 class gcStringValue {
 public:
     void Get(cHandlePairT<gcStringTable, cSubHandleT<gcString> > *) const;
+    void Write(cFile &) const;
+};
+
+class gcTableColumn {
+public:
+    void Write(cFile &) const;
+};
+
+class gcPartialEntityController {
+public:
+    void Write(cFile &) const;
+};
+
+class gcSubGeomController {
+public:
+    void Write(cFile &) const;
+};
+
+class gcDoSwitch {
+public:
+    void GetText(char *) const;
 };
 
 void gcStringValue::Get(cHandlePairT<gcStringTable, cSubHandleT<gcString> > *out) const {
     out->mHandle.mIndex = 0;
     cSubHandleT<gcString> *p = &out->mSubHandle;
     p->mIndex = 0;
+}
+
+void gcStringValue::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    wb.End();
+}
+
+void gcTableColumn::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    wb.End();
+}
+
+void gcPartialEntityController::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    wb.End();
+}
+
+void gcSubGeomController::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    wb.End();
+}
+
+void gcDoSwitch::GetText(char *buf) const {
+    cStrCat(buf, "away from %s )");
 }
