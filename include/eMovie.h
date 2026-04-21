729b95b23e78c4c561d9433e5060fd7f5494b034
autodecomp <autodecomp@localhost>
2026-04-21 08:42:51 -0500

    Match 5 functions (session c791afd3)
      - eMovie::Initialize
      - eMovie::Update
      - eMovie::Close
      - eMovie::eMovie
      - eMovie::Pause

--- include/eMovie.h
+++ include/eMovie.h
@@ -1,8 +1,15 @@
 #ifndef EMOVIE_H
 #define EMOVIE_H
 
+class cTimeValue;
+
 class eMovie {
 public:
+    eMovie(void);
+    static void Initialize(void);
+    void Close(void);
+    void Update(cTimeValue *);
+    static void Pause(void);
     static void SetDrawCallback(void (*callback)(void *), void *data);
     void PlatformPause(void);
     void PlatformResume(void);
--- src/eMovie.cpp
+++ src/eMovie.cpp
@@ -1,7 +1,52 @@
+#pragma control sched=1
+
 #include "eMovie.h"
 #include "eNavMesh.h"
 #include "eDynamicFluid.h"
 #include "eUser.h"
+
+extern eMovie *D_0037D2E0;
+extern int D_0037D2F0;
+
+extern "C" {
+    void eMoviePlatform___ct_eMoviePlatform_void(void *self);
+    void eMoviePlatform__Initialize_voidstatic(void);
+    void eMoviePlatform__Close_bool(void *self, bool force);
+    void eMoviePlatform__Update_cTimeValueptr(void *self, cTimeValue *tv);
+}
+
+eMovie::eMovie(void) {
+    __asm__ volatile("" ::: "memory");
+    ((int *)this)[0] = 0;
+    ((int *)this)[1] = 0;
+    eMoviePlatform___ct_eMoviePlatform_void((char *)this + 8);
+    D_0037D2E0 = this;
+}
+
+void eMovie::Initialize(void) {
+    eMoviePlatform__Initialize_voidstatic();
+}
+
+void eMovie::Close(void) {
+    __asm__ volatile("" ::: "memory");
+    eMoviePlatform__Close_bool((char *)this + 8, true);
+}
+
+void eMovie::Update(cTimeValue *tv) {
+    eMoviePlatform__Update_cTimeValueptr((char *)this + 8, tv);
+}
+
+void eMovie::Pause(void) {
+    eMovie *inst = D_0037D2E0;
+    if (inst != 0) {
+        int c = D_0037D2F0 + 1;
+        D_0037D2F0 = c;
+        __asm__ volatile("" ::: "memory");
+        if (c == 1) {
+            inst->PlatformPause();
+        }
+    }
+}
 
 void eMovie::PlatformResume(void) {
 }
