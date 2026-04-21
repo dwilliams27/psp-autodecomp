88319a99a23973c385359c7ce762e21e038acec8
autodecomp <autodecomp@localhost>
2026-04-21 17:55:05 -0500

    Match 5 functions (session 685c49df)
      - nwNetwork::GetInterfaceName
      - nwNetwork::CheckSecurity
      - nwNetwork::StopInterface
      - nwNetwork::GetSelectedInterface
      - nwNetwork::StartInterface

--- src/nwNetwork.cpp
+++ src/nwNetwork.cpp
@@ -1,8 +1,47 @@
+void cStrCopy(char *, const char *, int);
+
 class nwNetwork {
 public:
+    enum nwNetError {};
     static int GetLastPlatformError(void);
+    static void GetInterfaceName(int, char *, int);
+    static void CheckSecurity(void);
+    static void StopInterface(void);
+    static int GetSelectedInterface(void);
+    static void StartInterface(void);
+    static int GetNumInterfaces(void);
+    static void SetLastError(nwNetError);
 };
 
 int nwNetwork::GetLastPlatformError(void) {
     return 0;
+}
+
+void nwNetwork::GetInterfaceName(int, char *dest, int size) {
+    cStrCopy(dest, (const char *)0x371198, size);
+}
+
+void nwNetwork::CheckSecurity(void) {
+    SetLastError(nwNetError(0));
+    *(unsigned char *)0x37D949 = 1;
+}
+
+void nwNetwork::StopInterface(void) {
+    SetLastError(nwNetError(0));
+    *(unsigned char *)0x37D948 = 0;
+    *(unsigned char *)0x37D949 = 0;
+}
+
+void nwNetwork::StartInterface(void) {
+    SetLastError(nwNetError(0));
+    *(unsigned char *)0x37D948 = 1;
+    *(unsigned char *)0x37D949 = 0;
+}
+
+int nwNetwork::GetSelectedInterface(void) {
+    int selected = -1;
+    if (GetNumInterfaces() > 0) {
+        selected = 0;
+    }
+    return selected;
 }
