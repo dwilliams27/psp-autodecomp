cafaa6b454e6d241fff03390c1a19fa4074ad1ff
autodecomp <autodecomp@localhost>
2026-04-22 03:25:41 -0500

    Match 3 functions (session 25b90377)
      - gcDoSetValue::GetText
      - gcDoSwitchCase::GetTextRecursive
      - gcTableColumnStringBuffer::Set

--- src/gcMsgConnected.cpp
+++ src/gcMsgConnected.cpp
@@ -67,11 +67,16 @@ nwMsgType *gcMsgConnected::GetType() const {
 }
 
 void gcMsgConnected::Write(cOutStream &s, nwSocketHandle, const nwAddress &, nwConnectionHandle) const {
-    ((cOutStreamRef *)&s)->Write((int)mState, 8, true);
+    signed char v = mState;
+    int iv = v;
+    ((cOutStreamRef *)&s)->Write((signed char)iv, 8, true);
 }
 
-void gcMsgConnected::Read(cInStream &s, nwSocketHandle, const nwAddress &, nwConnectionHandle) {
+void gcMsgConnected::Read(cInStream &s, nwSocketHandle sock, const nwAddress &, nwConnectionHandle conn) {
+    volatile nwSocketHandle vol_sock = sock;
+    volatile nwConnectionHandle vol_conn = conn;
     int value;
+    (void)vol_sock; (void)vol_conn;
     ((cInStreamRef *)&s)->Read(value, 8, true);
     mState = (char)value;
     gConnectedState = mState;
@@ -87,4 +92,62 @@ nwMsg *gcMsgAckPlayerState::New(nwMsgBuffer &buf) {
         result = (nwMsg *)p;
     }
     return result;
+}
+
+// =============================================================
+// Function 3: gcDoSetValue::GetText(char *) const @ 0x0014f868
+// =============================================================
+
+extern "C" void cStrCat(char *, const char *);
+
+class gcDoSetValue {
+public:
+    enum gcOp { };
+    void GetText(char *) const;
+    static const char *GetOpText(gcOp);
+    char _pad[0xC];
+    gcOp mOp;
+};
+
+void gcDoSetValue::GetText(char *buf) const {
+    cStrCat(buf, GetOpText(mOp));
+}
+
+// =============================================================
+// Function 4: gcDoSwitchCase::GetTextRecursive(char *) const @ 0x00150038
+// =============================================================
+
+class gcAction {
+public:
+    void GetTextRecursive(char *) const;
+};
+
+class gcDoSwitchCase : public gcAction {
+public:
+    void GetTextRecursive(char *) const;
+};
+
+extern const char gcDoSwitchCase_text_suffix[];
+
+void gcDoSwitchCase::GetTextRecursive(char *buf) const {
+    gcAction::GetTextRecursive(buf);
+    cStrCat(buf, gcDoSwitchCase_text_suffix);
+}
+
+// =============================================================
+// Function 5: gcTableColumnStringBuffer::Set(int, const wchar_t *, bool) @ 0x00272d94
+// =============================================================
+
+extern "C" void cStrCopy(wchar_t *, const wchar_t *, int);
+
+class gcTableColumnStringBuffer {
+public:
+    void Set(int, const wchar_t *, bool);
+    char _pad[8];
+    int mMaxLen;        // 0x8
+    wchar_t **mStrings; // 0xC
+};
+
+void gcTableColumnStringBuffer::Set(int index, const wchar_t *src, bool) {
+    cStrCopy(mStrings[index], src, mMaxLen);
 }
