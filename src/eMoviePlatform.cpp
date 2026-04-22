0de78334d5680729ee5d55ebbb9a2d664ed35b55
autodecomp <autodecomp@localhost>
2026-04-22 18:35:29 -0500

    Match 4 functions (session 013bfa7a)
      - eMoviePlatform::dispbuf_getDrawbuf
      - eMoviePlatform::dispbuf_checkDecodeEnd
      - eMoviePlatform::soundbuf_getDrawbuf
      - eMoviePlatform::read_isFull

--- src/eMoviePlatform.cpp
+++ src/eMoviePlatform.cpp
@@ -29,14 +29,21 @@ public:
     void dispbuf_setPts(unsigned int);
     unsigned int dispbuf_getPts(void);
     unsigned int soundbuf_getPts(void);
+    int dispbuf_getDrawbuf(void);
+    int dispbuf_checkDecodeEnd(void);
+    int soundbuf_getDrawbuf(void);
 
     void avsync_delete(void);
     int avsync_video_getPts(void);
+    void avsync_video_setPts(unsigned int);
     static void OnSuspend(void *);
     void Close(bool);
     void control_setCondition(unsigned int);
     int control_getCondition(void);
     void read_checkConditionEnd(void);
+    int read_isEnd(void);
+    int read_getCapacity(void);
+    int read_isFull(void);
 };
 
 int eMoviePlatform::dispbuf_getCapacity(void) {
@@ -87,4 +94,51 @@ void eMoviePlatform::read_checkConditionEnd(void) {
     if ((control_getCondition() & 0xff) == 0xff) {
         m_read_state = 3;
     }
+}
+
+int eMoviePlatform::dispbuf_getDrawbuf(void) {
+    int result = 0;
+    if (dispbuf_getCapacity() > 0) {
+        int idx = *(int *)((char *)this + 0x2E0);
+        result = *(int *)((char *)this + idx * 4 + 0x2D0);
+    }
+    return result;
+}
+
+int eMoviePlatform::dispbuf_checkDecodeEnd(void) {
+    if (control_getCondition() & 8) {
+        *(int *)((char *)this + 0x2F4) = 0xFF;
+        return 1;
+    }
+    return 0;
+}
+
+int eMoviePlatform::soundbuf_getDrawbuf(void) {
+    int result = 0;
+    if (soundbuf_getCapacity() > 0) {
+        int idx = *(int *)((char *)this + 0x328);
+        result = *(int *)((char *)this + idx * 4 + 0x308);
+    }
+    return result;
+}
+
+void eMoviePlatform::avsync_video_setPts(unsigned int pts) {
+    if (pts == (unsigned int)-1) {
+        pts = *(unsigned int *)((char *)this + 0x378) + *(unsigned int *)((char *)this + 0x37C);
+    }
+    int idx = *(int *)((char *)this + 0x368);
+    int size = *(int *)((char *)this + 0x374);
+    *(unsigned int *)((char *)this + 0x378) = pts;
+    int count = *(int *)((char *)this + 0x370);
+    *(unsigned int *)((char *)this + idx * 4 + 0x358) = pts;
+    *(int *)((char *)this + 0x370) = count + 1;
+    *(int *)((char *)this + 0x368) = (idx + 1) % size;
+}
+
+int eMoviePlatform::read_isFull(void) {
+    control_getCondition();
+    if (read_isEnd()) {
+        return 1;
+    }
+    return read_getCapacity() == 0;
 }
