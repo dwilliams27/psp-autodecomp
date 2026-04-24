15bd3cdbff8e6cd48b715bdad70a00a9c428831d
autodecomp <autodecomp@localhost>
2026-04-24 17:47:17 -0500

    Match 1 functions (session 6ef107cd)
      - cWriteBlock::Write

--- src/cWriteBlock.cpp
+++ src/cWriteBlock.cpp
@@ -1,8 +1,32 @@
+struct cFileHandle;
+
+class cFile {
+public:
+    cFileHandle *mHandle;
+};
+
+class cFileSystem {
+public:
+    static int Write(cFileHandle *, const void *, unsigned int);
+};
+
 class cWriteBlock {
 public:
+    cFile *mFile;
+
+    void Write(char);
     void Write(int, const short *);
+    void Write(int, const unsigned short *);
     void Write(int, const wchar_t *);
 };
+
+void cWriteBlock::Write(char data) {
+    cFileSystem::Write(mFile->mHandle, &data, 1);
+}
+
+void cWriteBlock::Write(int count, const unsigned short *data) {
+    Write(count, (const short *)data);
+}
 
 void cWriteBlock::Write(int count, const wchar_t *data) {
     Write(count, (const short *)data);
