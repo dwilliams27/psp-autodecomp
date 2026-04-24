5ac92c0a50869d3c8aa47f99c941d3eec1838597
autodecomp <autodecomp@localhost>
2026-04-24 17:50:53 -0500

    Match 1 functions (session 227e1713)
      - cWriteBlock::Write

--- src/cWriteBlock.cpp
+++ src/cWriteBlock.cpp
@@ -15,6 +15,8 @@ public:
     cFile *mFile;
 
     void Write(char);
+    void Write(unsigned char);
+    void Write(int, const char *);
     void Write(int, const short *);
     void Write(int, const unsigned short *);
     void Write(int, const wchar_t *);
@@ -22,6 +24,14 @@ public:
 
 void cWriteBlock::Write(char data) {
     cFileSystem::Write(mFile->mHandle, &data, 1);
+}
+
+void cWriteBlock::Write(unsigned char data) {
+    cFileSystem::Write(mFile->mHandle, &data, 1);
+}
+
+void cWriteBlock::Write(int count, const char *data) {
+    cFileSystem::Write(mFile->mHandle, data, count);
 }
 
 void cWriteBlock::Write(int count, const unsigned short *data) {
