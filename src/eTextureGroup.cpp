d238ea4136a044b14001fa8216f1e6f660e03887
autodecomp <autodecomp@localhost>
2026-04-26 02:13:34 -0500

    Match 5 functions (session 65c65f78)
      - eTextureGroup::AssignCopy
      - eMaterialGroup::AssignCopy
      - ePathGroup::IsManagedTypeExternal
      - eMeshGroup::IsManagedTypeExternal
      - eGeomTemplateGroup::IsManagedTypeExternal

--- src/eTextureGroup.cpp
+++ src/eTextureGroup.cpp
@@ -4,14 +4,24 @@
 
 class eTextureGroup {
 public:
+    char _pad0[8];
+    unsigned char mFlag;
+    char _pad1[3];
+    int mField;
     static bool IsManagedTypeExternalStatic();
     bool IsManagedTypeExternal() const;
+    void AssignCopy(const class cBase *);
 };
 
 class eMaterialGroup {
 public:
+    char _pad0[8];
+    unsigned char mFlag;
+    char _pad1[3];
+    int mField;
     static bool IsManagedTypeExternalStatic();
     bool IsManagedTypeExternal() const;
+    void AssignCopy(const class cBase *);
 };
 
 class eMaterialSetGroup {
@@ -32,8 +42,42 @@ public:
     bool IsManagedTypeExternal() const;
 };
 
+class ePathGroup {
+public:
+    static bool IsManagedTypeExternalStatic();
+    bool IsManagedTypeExternal() const;
+};
+
+class eMeshGroup {
+public:
+    static bool IsManagedTypeExternalStatic();
+    bool IsManagedTypeExternal() const;
+};
+
+class eGeomTemplateGroup {
+public:
+    static bool IsManagedTypeExternalStatic();
+    bool IsManagedTypeExternal() const;
+};
+
+class cBase;
+
+template <class T> T *dcast(const cBase *);
+
+void eTextureGroup::AssignCopy(const cBase *base) {
+    eTextureGroup *src = dcast<eTextureGroup>(base);
+    mFlag = src->mFlag;
+    mField = src->mField;
+}
+
 bool eTextureGroup::IsManagedTypeExternal() const {
     return IsManagedTypeExternalStatic();
+}
+
+void eMaterialGroup::AssignCopy(const cBase *base) {
+    eMaterialGroup *src = dcast<eMaterialGroup>(base);
+    mFlag = src->mFlag;
+    mField = src->mField;
 }
 
 bool eMaterialGroup::IsManagedTypeExternal() const {
@@ -49,5 +93,17 @@ bool eSurfaceGroup::IsManagedTypeExternal() const {
 }
 
 bool eSpriteGroup::IsManagedTypeExternal() const {
+    return IsManagedTypeExternalStatic();
+}
+
+bool ePathGroup::IsManagedTypeExternal() const {
+    return IsManagedTypeExternalStatic();
+}
+
+bool eMeshGroup::IsManagedTypeExternal() const {
+    return IsManagedTypeExternalStatic();
+}
+
+bool eGeomTemplateGroup::IsManagedTypeExternal() const {
     return IsManagedTypeExternalStatic();
 }
