353ce613bc7de664667f228ebaecff50608b76d0
autodecomp <autodecomp@localhost>
2026-04-25 20:28:51 -0500

    Match 1 functions (session 4f7f1d0f)
      - gcDesiredUIWidget::GetText

--- src/gcDesiredUIWidget.cpp
+++ src/gcDesiredUIWidget.cpp
@@ -1,11 +1,23 @@
 class cObject;
 
+struct gcDesiredUIWidgetHelper {
+    int _a;
+    int _b;
+    int _c;
+    void GetText(char *) const;
+};
+
 class gcDesiredUIWidget {
 public:
     cObject *Get(bool) const;
     cObject *GetObject(bool) const;
+    void GetText(char *) const;
 };
 
 cObject *gcDesiredUIWidget::GetObject(bool b) const {
     return Get(b);
+}
+
+void gcDesiredUIWidget::GetText(char *buf) const {
+    ((gcDesiredUIWidgetHelper *)((char *)this + 0xC))->GetText(buf);
 }
