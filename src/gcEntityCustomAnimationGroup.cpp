04e42390f4293720740ab9cba1115c155c12e41a
autodecomp <autodecomp@localhost>
2026-04-26 14:10:02 -0500

    Match 5 functions (session fa29dee2)
      - gcEntityCustomAnimationGroup::IsManagedTypeExternal
      - eInput::RecheckPresent
      - gcUI::Reset
      - gcRigidBodyController::OnDeselected
      - gcValLobbyInfo::AssignCopy

--- src/gcEntityCustomAnimationGroup.cpp
+++ src/gcEntityCustomAnimationGroup.cpp
@@ -8,6 +8,63 @@ public:
     unsigned char mFlag;
     char _pad1[3];
     int mField;
+    static bool IsManagedTypeExternalStatic();
+    bool IsManagedTypeExternal() const;
+    void AssignCopy(const cBase *);
+};
+
+class eInputJoystick {
+public:
+    static void RecheckPresent();
+};
+
+class eInputMouse {
+public:
+    static void RecheckPresent();
+};
+
+class eInputKeyboard {
+public:
+    static void RecheckPresent();
+};
+
+namespace eInput {
+    void RecheckPresent();
+}
+
+class gcUI {
+public:
+    class gcFader {
+    public:
+        char _pad0[0x24];
+        void Reset();
+    };
+
+    char _pad0[8];
+    gcFader mFader0;
+    gcFader mFader1;
+
+    void Reset();
+};
+
+class cType;
+
+class gcEntityController {
+public:
+    void OnDeselected();
+    void SetPhysicsController(const cType *);
+};
+
+class gcRigidBodyController : public gcEntityController {
+public:
+    void OnDeselected();
+};
+
+class gcValLobbyInfo {
+public:
+    int _pad0;
+    int _pad4;
+    int mValue;
     void AssignCopy(const cBase *);
 };
 
@@ -15,4 +72,29 @@ void gcEntityCustomAnimationGroup::AssignCopy(const cBase *base) {
     gcEntityCustomAnimationGroup *src = dcast<gcEntityCustomAnimationGroup>(base);
     mFlag = src->mFlag;
     mField = src->mField;
+}
+
+bool gcEntityCustomAnimationGroup::IsManagedTypeExternal() const {
+    return IsManagedTypeExternalStatic();
+}
+
+void eInput::RecheckPresent() {
+    eInputJoystick::RecheckPresent();
+    eInputMouse::RecheckPresent();
+    eInputKeyboard::RecheckPresent();
+}
+
+void gcUI::Reset() {
+    mFader0.Reset();
+    mFader1.Reset();
+}
+
+void gcRigidBodyController::OnDeselected() {
+    gcEntityController::OnDeselected();
+    gcEntityController::SetPhysicsController(0);
+}
+
+void gcValLobbyInfo::AssignCopy(const cBase *base) {
+    gcValLobbyInfo *src = dcast<gcValLobbyInfo>(base);
+    mValue = src->mValue;
 }
