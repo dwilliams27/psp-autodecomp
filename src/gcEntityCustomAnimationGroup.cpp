8b76d05489bfcdc46f7487dc2bcc92caba232a3e
autodecomp <autodecomp@localhost>
2026-04-27 02:52:13 -0500

    Match 2 functions (session ca6c8888)
      - gcEntityCustomAnimationGroup::Write
      - gcEntityCustomAnimationGroup::~gcEntityCustomAnimationGroup

--- src/gcEntityCustomAnimationGroup.cpp
+++ src/gcEntityCustomAnimationGroup.cpp
@@ -1,17 +1,64 @@
 class cBase;
+class cFile;
+class cMemPool;
 
 template <class T> T *dcast(const cBase *);
 
-class gcEntityCustomAnimationGroup {
+class cWriteBlock {
 public:
-    char _pad0[8];
+    int _data[2];
+    cWriteBlock(cFile &, unsigned int);
+    void End(void);
+};
+
+class cReadBlock {
+public:
+    int _data[5];
+    cReadBlock(cFile &, unsigned int, bool);
+    ~cReadBlock(void);
+};
+
+extern "C" void cFile_SetCurrentPos(void *, unsigned int);
+
+struct DeleteRecord {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
+};
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
+
+class cGroup {
+public:
+    int _pad[2];
+    ~cGroup();
+    void Write(cFile &) const;
+    int Read(cFile &, cMemPool *);
+};
+
+class gcEntityCustomAnimationGroup : public cGroup {
+public:
     unsigned char mFlag;
-    char _pad1[3];
     int mField;
+    char _pad1[3];
     static bool IsManagedTypeExternalStatic();
     bool IsManagedTypeExternal() const;
     void AssignCopy(const cBase *);
+    void Write(cFile &) const;
+    int Read(cFile &, cMemPool *);
+    ~gcEntityCustomAnimationGroup();
+    static void operator delete(void *p) {
+        cMemPool *pool = cMemPool::GetPoolFromPtr(p);
+        char *block = ((char **)pool)[9];
+        DeleteRecord *rec = (DeleteRecord *)(((char **)block)[7] + 0x30);
+        rec->fn(block + rec->offset, p);
+    }
 };
+
+extern char gcEntityCustomAnimationGroupvirtualtable[];
 
 class eInputJoystick {
 public:
@@ -62,8 +109,8 @@ public:
 
 class gcValLobbyInfo {
 public:
-    int _pad0;
     int _pad4;
+    int _pad0;
     int mValue;
     void AssignCopy(const cBase *);
 };
@@ -76,6 +123,30 @@ void gcEntityCustomAnimationGroup::AssignCopy(const cBase *base) {
 
 bool gcEntityCustomAnimationGroup::IsManagedTypeExternal() const {
     return IsManagedTypeExternalStatic();
+}
+
+// gcEntityCustomAnimationGroup::Write(cFile &) const @ 0x000d2098
+void gcEntityCustomAnimationGroup::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    this->cGroup::Write(file);
+    wb.End();
+}
+
+// gcEntityCustomAnimationGroup::Read(cFile &, cMemPool *) @ 0x000d20e4
+int gcEntityCustomAnimationGroup::Read(cFile &file, cMemPool *pool) {
+    int result;
+    cReadBlock rb(file, 1, true);
+    __asm__ volatile("ori %0, $0, 1" : "=r"(result));
+    if ((unsigned int)rb._data[3] == 1 && this->cGroup::Read(file, pool)) goto success;
+    cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+    return 0;
+success:
+    return result;
+}
+
+// gcEntityCustomAnimationGroup::~gcEntityCustomAnimationGroup(void) @ 0x00238080
+gcEntityCustomAnimationGroup::~gcEntityCustomAnimationGroup() {
+    *(void **)((char *)this + 4) = gcEntityCustomAnimationGroupvirtualtable;
 }
 
 void eInput::RecheckPresent() {
