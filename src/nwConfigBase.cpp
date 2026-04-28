e85d46532a32d8ed2585638502fce1b3d4b04904
autodecomp <autodecomp@localhost>
2026-04-28 16:30:33 -0500

    Match 2 functions (session c034cef3)
      - nwConfigBase::~nwConfigBase
      - nwConfigBase::nwConfigBase

--- src/nwConfigBase.cpp
+++ src/nwConfigBase.cpp
@@ -1,7 +1,13 @@
 class cBase;
 class cFile;
-class cMemPool;
 class cType;
+
+inline void *operator new(unsigned int, void *p) { return p; }
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
 
 class cWriteBlock {
 public:
@@ -22,16 +28,35 @@ public:
                                  const char *, const char *, unsigned int);
 };
 
-class nwConfigBase {
-public:
-    void Write(cFile &) const;
-    static cBase *New(cMemPool *, cBase *);
-    const cType *GetType(void) const;
+struct DeleteRec {
+    short offset;
+    short _pad;
+    void (*fn)(void *, void *);
 };
 
-extern "C" {
-    void nwConfigBase__nwConfigBase_cBaseptr(void *self, cBase *parent);
-}
+class nwConfigBase {
+public:
+    nwConfigBase(cBase *);
+    ~nwConfigBase();
+
+    void Write(cFile &) const;
+    void AssignCopy(const cBase *);
+    static cBase *New(cMemPool *, cBase *);
+    const cType *GetType(void) const;
+
+    static void operator delete(void *p) {
+        cMemPool *pool = cMemPool::GetPoolFromPtr(p);
+        char *block = ((char **)pool)[9];
+        DeleteRec *rec = (DeleteRec *)(((char **)block)[7] + 0x30);
+        short off = rec->offset;
+        void (*fn)(void *, void *) = rec->fn;
+        fn(block + off, p);
+    }
+};
+
+void cStrCopy(char *, const char *, int);
+
+extern "C" nwConfigBase *dcastdcast_nwConfigBaseptr__constcBaseptr__0036B228(const cBase *);
 
 extern cType *D_000385DC;
 extern cType *D_0009F928;
@@ -43,6 +68,28 @@ struct AllocRec {
     short _pad;
     void *(*fn)(void *, int, int, int, int);
 };
+
+// nwConfigBase::nwConfigBase(cBase *) @ 0x001a4eec
+nwConfigBase::nwConfigBase(cBase *parent) {
+    *(cBase **)this = parent;
+    *(void **)((char *)this + 4) = (void *)0x38D8D8;
+    *(int *)((char *)this + 8) = 0x65;
+    *(unsigned short *)((char *)this + 0xC) = 0x2694;
+    *(int *)((char *)this + 0x10) = 0x4000;
+    *(char *)((char *)this + 0x14) = 0;
+    *(int *)((char *)this + 0x18) = 0;
+    *(int *)((char *)this + 0x1C) = 0x29000;
+    *(nwConfigBase **)0x37D94C = this;
+    cStrCopy((char *)this + 0x20, (const char *)0x371168, 0x20);
+    cStrCopy((char *)this + 0x40, (const char *)0x371170, 0x20);
+}
+
+// nwConfigBase::~nwConfigBase(void) @ 0x001a4f7c
+nwConfigBase::~nwConfigBase() {
+    *(void **)((char *)this + 4) = (void *)0x38D8D8;
+    *(nwConfigBase **)0x37D94C = 0;
+    *(void **)((char *)this + 4) = (void *)0x37E6A8;
+}
 
 // nwConfigBase::Write(cFile &) const @ 0x001a4d00
 void nwConfigBase::Write(cFile &file) const {
@@ -68,10 +115,40 @@ cBase *nwConfigBase::New(cMemPool *pool, cBase *parent) {
     nwConfigBase *result = 0;
     nwConfigBase *obj = (nwConfigBase *)rec->fn(base, 0x60, 4, 0, 0);
     if (obj != 0) {
-        nwConfigBase__nwConfigBase_cBaseptr(obj, parent);
+        new (obj) nwConfigBase(parent);
         result = obj;
     }
     return (cBase *)result;
+}
+
+// nwConfigBase::AssignCopy(const cBase *) @ 0x0036b340
+void nwConfigBase::AssignCopy(const cBase *src) {
+    nwConfigBase *other =
+        dcastdcast_nwConfigBaseptr__constcBaseptr__0036B228(src);
+
+    int *self_i = (int *)this;
+    int *other_i = (int *)other;
+
+    self_i[2] = other_i[2];
+    *(unsigned short *)((char *)this + 0xC) =
+        *(unsigned short *)((char *)other + 0xC);
+    self_i[4] = other_i[4];
+    *(unsigned char *)((char *)this + 0x14) =
+        *(unsigned char *)((char *)other + 0x14);
+    self_i[6] = other_i[6];
+    self_i[7] = other_i[7];
+
+    int i = 0;
+    do {
+        *((char *)this + (0x20 + i)) = *((char *)other + 0x20 + i);
+        i += 1;
+    } while (i < 0x20);
+
+    i = 0;
+    do {
+        *((char *)this + (0x40 + i)) = *((char *)other + 0x40 + i);
+        i += 1;
+    } while (i < 0x20);
 }
 
 // nwConfigBase::GetType(void) const @ 0x0036b3e4
