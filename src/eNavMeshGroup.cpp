b88924e8a873fb0fb989d30c766b533be047cea0
autodecomp <autodecomp@localhost>
2026-04-28 12:15:45 -0500

    Match 1 functions (session 2e41ada3)
      - eNavMeshGroup::~eNavMeshGroup

--- src/eNavMeshGroup.cpp
+++ src/eNavMeshGroup.cpp
@@ -4,12 +4,32 @@ class cMemPool;
 
 template <class T> T *dcast(const cBase *);
 
+struct DeleteRecord {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
+};
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
+
 class cWriteBlock {
 public:
     int _data[2];
     cWriteBlock(cFile &, unsigned int);
     void End();
 };
+
+class cReadBlock {
+public:
+    int _data[5];
+    cReadBlock(cFile &, unsigned int, bool);
+    ~cReadBlock();
+};
+
+void cFile_SetCurrentPos(void *, unsigned int);
 
 class cGroup {
 public:
@@ -18,16 +38,30 @@ public:
     unsigned char mFlag;    // 0x08
     char _pad[3];
     int mField;             // 0x0C
+    ~cGroup();
     void Write(cFile &) const;
+    int Read(cFile &, cMemPool *);
 };
 
 class eNavMeshGroup : public cGroup {
 public:
+    ~eNavMeshGroup();
     void AssignCopy(const cBase *);
+    int Read(cFile &, cMemPool *);
     void Write(cFile &) const;
     bool IsManagedTypeExternal() const;
     static bool IsManagedTypeExternalStatic();
+    static void operator delete(void *p) {
+        cMemPool *pool = cMemPool::GetPoolFromPtr(p);
+        char *block = ((char **)pool)[9];
+        DeleteRecord *rec = (DeleteRecord *)(((char **)block)[7] + 0x30);
+        short off = rec->offset;
+        void (*fn)(void *, void *) = rec->fn;
+        fn(block + off, p);
+    }
 };
+
+extern char eNavMeshGroupvirtualtable[];
 
 void eNavMeshGroup::AssignCopy(const cBase *base) {
     eNavMeshGroup *src = dcast<eNavMeshGroup>(base);
@@ -43,4 +77,19 @@ void eNavMeshGroup::Write(cFile &file) const {
     cWriteBlock wb(file, 1);
     cGroup::Write(file);
     wb.End();
+}
+
+int eNavMeshGroup::Read(cFile &file, cMemPool *pool) {
+    register int result __asm__("$19");
+    cReadBlock rb(file, 1, true);
+    __asm__ volatile("ori %0, $0, 1" : "=r"(result));
+    if ((unsigned int)rb._data[3] == 1 && this->cGroup::Read(file, pool)) goto success;
+    cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+    return 0;
+success:
+    return result;
+}
+
+eNavMeshGroup::~eNavMeshGroup() {
+    *(void **)((char *)this + 4) = eNavMeshGroupvirtualtable;
 }
