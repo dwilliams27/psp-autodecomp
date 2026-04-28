85eea395290a6b3a2d740be66f0531396220660d
autodecomp <autodecomp@localhost>
2026-04-28 03:10:12 -0500

    Match 1 functions (session 27b32c2b)
      - cWriteBlock::Write

--- src/cWriteBlock.cpp
+++ src/cWriteBlock.cpp
@@ -14,14 +14,19 @@ class cWriteBlock {
 public:
     cFile *mFile;
 
+    void Write(bool);
     void Write(char);
     void Write(unsigned char);
+    void Write(short);
+    void Write(int, const bool *);
     void Write(int, const char *);
     void Write(int, const unsigned char *);
     void Write(int, const short *);
     void Write(int, const unsigned short *);
     void Write(int, const wchar_t *);
 };
+
+extern unsigned char gByteSwap;
 
 void cWriteBlock::Write(char data) {
     cFileSystem::Write(mFile->mHandle, &data, 1);
@@ -45,4 +50,21 @@ void cWriteBlock::Write(int count, const unsigned short *data) {
 
 void cWriteBlock::Write(int count, const wchar_t *data) {
     Write(count, (const short *)data);
+}
+
+void cWriteBlock::Write(short data) {
+    short tmp;
+    if (gByteSwap) {
+        tmp = (short)(((data & 0xff00) >> 8) | ((data & 0xff) << 8));
+    } else {
+        tmp = data;
+    }
+    data = tmp;
+    cFileSystem::Write(mFile->mHandle, &data, 2);
+}
+
+void cWriteBlock::Write(int count, const bool *data) {
+    for (int i = 0; i < count; i++) {
+        Write(data[i]);
+    }
 }
