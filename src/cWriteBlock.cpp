e38c9e9abe60f38b62b39572267622bbc970cd1a
autodecomp <autodecomp@localhost>
2026-04-28 16:46:13 -0500

    Match 3 functions (session f0b6dc45)
      - cWriteBlock::Write
      - cWriteBlock::Write
      - cWriteBlock::Write

--- src/cWriteBlock.cpp
+++ src/cWriteBlock.cpp
@@ -18,10 +18,13 @@ public:
     void Write(char);
     void Write(unsigned char);
     void Write(short);
+    void Write(unsigned short);
+    void Write(wchar_t);
     void Write(float);
     void Write(int, const bool *);
     void Write(int, const char *);
     void Write(int, const unsigned char *);
+    void Write(int, const int *);
     void Write(int, const short *);
     void Write(int, const unsigned short *);
     void Write(int, const wchar_t *);
@@ -64,6 +67,30 @@ void cWriteBlock::Write(short data) {
     cFileSystem::Write(mFile->mHandle, &data, 2);
 }
 
+void cWriteBlock::Write(unsigned short data) {
+    unsigned short tmp;
+    if (gByteSwap) {
+        int val = data & 0xffff;
+        tmp = (unsigned short)(((val & 0xff00) >> 8) | ((val & 0xff) << 8));
+    } else {
+        tmp = data;
+    }
+    data = tmp;
+    cFileSystem::Write(mFile->mHandle, &data, 2);
+}
+
+void cWriteBlock::Write(wchar_t data) {
+    wchar_t tmp;
+    if (gByteSwap) {
+        int val = data & 0xffff;
+        tmp = (wchar_t)(((val & 0xff00) >> 8) | ((val & 0xff) << 8));
+    } else {
+        tmp = data;
+    }
+    data = tmp;
+    cFileSystem::Write(mFile->mHandle, &data, 2);
+}
+
 void cWriteBlock::Write(float data) {
     unsigned int u = *(unsigned int *)&data;
     if (gByteSwap) {
@@ -73,6 +100,24 @@ void cWriteBlock::Write(float data) {
     }
     int bits = (int)u;
     cFileSystem::Write(mFile->mHandle, &bits, 4);
+}
+
+void cWriteBlock::Write(int count, const int *data) {
+    if (!gByteSwap) {
+        cFileSystem::Write(mFile->mHandle, data, count * 4);
+    } else {
+        while (count > 0) {
+            int val = *data;
+            int hi = ((val & 0xFF000000) >> 24) | ((val & 0xFF0000) >> 8);
+            int mid = (val & 0xFF00) << 8;
+            hi = hi | mid;
+            int lo = (val & 0xFF) << 24;
+            int tmp = hi | lo;
+            cFileSystem::Write(mFile->mHandle, &tmp, 4);
+            count--;
+            data++;
+        }
+    }
 }
 
 void cWriteBlock::Write(int count, const short *data) {
