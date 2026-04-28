f432068e3188bfd6e0a41241eb6f73f6a78d5ae7
autodecomp <autodecomp@localhost>
2026-04-28 02:36:49 -0500

    Match 1 functions (session 8edd3319)
      - gcValUIOpacity::~gcValUIOpacity

--- src/gcValUIOpacity.cpp
+++ src/gcValUIOpacity.cpp
@@ -2,7 +2,9 @@
 // Methods in this file:
 //   0x0036357c  New(cMemPool *, cBase *) static
 //   0x00363768  Write(cFile &) const
+//   0x003637c0  Read(cFile &, cMemPool *)
 //   0x00363bc8  VisitReferences(unsigned int, cBase *, void (*)(cBase *, unsigned int, void *), void *, unsigned int)
+//   0x00363c48  ~gcValUIOpacity(void)
 //
 // Class layout (20 bytes, alloc size 0x14):
 //   [0x00] mParent (cBase *)
@@ -20,11 +22,21 @@ public:
     void End(void);
 };
 
+class cReadBlock {
+public:
+    unsigned int _data[5];
+    cReadBlock(cFile &, unsigned int, bool);
+    ~cReadBlock(void);
+};
+
+void cFile_SetCurrentPos(void *, unsigned int);
+
 struct gcDesiredUIWidgetHelper {
     int _a;
     int _b;
     int _c;
     void Write(cWriteBlock &) const;
+    void Read(cReadBlock &);
     void VisitReferences(unsigned int, cBase *, void (*)(cBase *, unsigned int, void *), void *, unsigned int);
 };
 
@@ -32,6 +44,18 @@ void gcDesiredUIWidgetHelper_ctor(void *, int);
 
 extern char gcLValuevirtualtable[];
 extern char gcValUIOpacityvirtualtable[];
+extern char cBaseclassdesc[];
+
+struct DeleteRecord {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
+};
+
+class cMemPoolNS {
+public:
+    static cMemPoolNS *GetPoolFromPtr(const void *);
+};
 
 class gcLValue {
 public:
@@ -39,15 +63,27 @@ public:
     void *mVtable;
     gcLValue(cBase *parent);
     void Write(cFile &) const;
+    int Read(cFile &, cMemPool *);
 };
 
 class gcValUIOpacity : public gcLValue {
 public:
     gcDesiredUIWidgetHelper mHelper;
 
+    ~gcValUIOpacity();
     void Write(cFile &) const;
+    int Read(cFile &, cMemPool *);
     void VisitReferences(unsigned int, cBase *, void (*)(cBase *, unsigned int, void *), void *, unsigned int);
     static cBase *New(cMemPool *, cBase *);
+
+    static void operator delete(void *p) {
+        cMemPoolNS *pool = cMemPoolNS::GetPoolFromPtr(p);
+        char *block = ((char **)pool)[9];
+        DeleteRecord *rec = (DeleteRecord *)(((char **)block)[7] + 0x30);
+        short off = rec->offset;
+        void (*fn)(void *, void *) = rec->fn;
+        fn(block + off, p);
+    }
 };
 
 inline gcLValue::gcLValue(cBase *parent) {
@@ -90,6 +126,24 @@ void gcValUIOpacity::Write(cFile &file) const {
     gcLValue::Write(file);
     ((gcDesiredUIWidgetHelper *)((char *)this + 8))->Write(wb);
     wb.End();
+}
+
+// ── gcValUIOpacity::Read(cFile &, cMemPool *) @ 0x003637c0 ──
+int gcValUIOpacity::Read(cFile &file, cMemPool *pool) {
+    int result;
+    cReadBlock rb(file, 2, true);
+    __asm__ volatile("ori %0, $0, 1" : "=r"(result));
+    if ((unsigned int)rb._data[3] == 2 && this->gcLValue::Read(file, pool)) goto success;
+    cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+    return 0;
+success:
+    ((gcDesiredUIWidgetHelper *)((char *)this + 8))->Read(rb);
+    return result;
+}
+
+// ── gcValUIOpacity::~gcValUIOpacity(void) @ 0x00363c48 ──
+gcValUIOpacity::~gcValUIOpacity() {
+    *(void **)((char *)this + 4) = cBaseclassdesc;
 }
 
 // ── gcValUIOpacity::VisitReferences @ 0x00363bc8 ──
