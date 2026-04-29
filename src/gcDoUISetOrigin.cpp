6ed18c6cb4ae437b2bac3345954bdce8f3a6328f
autodecomp <autodecomp@localhost>
2026-04-29 12:18:53 -0500

    Match 1 functions (session a909265e)
      - gcDoUISetOrigin::GetType

--- src/gcDoUISetOrigin.cpp
+++ src/gcDoUISetOrigin.cpp
@@ -2,6 +2,25 @@
 
 class cFile;
 class cMemPool;
+class cType;
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
+
+struct DeleteRecord {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
+};
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int, const cType *,
+                                 cBase *(*)(cMemPool *, cBase *), const char *, const char *,
+                                 unsigned int);
+};
 
 class cWriteBlock {
 public:
@@ -21,13 +40,22 @@ struct gcDesiredUIWidgetHelper {
     void VisitReferences(unsigned int, cBase *, void (*)(cBase *, unsigned int, void *), void *, unsigned int);
 };
 
-class gcDoUISetOrigin {
+class gcDoUISetOrigin : public gcAction {
 public:
+    ~gcDoUISetOrigin();
+    const cType *GetType(void) const;
     void GetText(char *) const;
     void Write(cFile &) const;
     void AssignCopy(const cBase *);
     void VisitReferences(unsigned int, cBase *, void (*)(cBase *, unsigned int, void *), void *, unsigned int);
     static cBase *New(cMemPool *, cBase *);
+
+    static void operator delete(void *p) {
+        cMemPool *pool = cMemPool::GetPoolFromPtr(p);
+        char *block = ((char **)pool)[9];
+        DeleteRecord *rec = (DeleteRecord *)(((char **)block)[7] + 0x30);
+        rec->fn(block + rec->offset, p);
+    }
 };
 
 void cStrAppend(char *, const char *, ...);
@@ -39,6 +67,13 @@ void gcDesiredUIWidgetHelper_ctor(void *, int);
 extern char gcDoUISetOriginvirtualtable[];
 extern const char gcDoUISetOrigin_fmt[];
 extern const char gcDoUISetOrigin_str2[];
+extern const char gcDoUISetOrigin_base_name[] asm("D_0036D894");
+extern const char gcDoUISetOrigin_base_desc[] asm("D_0036D89C");
+
+static cType *type_base asm("D_000385DC");
+static cType *type_expression asm("D_000385D8");
+static cType *type_action asm("D_000385D4");
+static cType *type_gcDoUISetOrigin asm("D_0009F73C");
 
 struct PoolBlock {
     char pad[0x1C];
@@ -50,6 +85,28 @@ struct AllocEntry {
     short pad;
     void *(*fn)(void *, int, int, int, int);
 };
+
+// 0x0030fd44, 280B
+const cType *gcDoUISetOrigin::GetType(void) const {
+    if (!type_gcDoUISetOrigin) {
+        if (!type_action) {
+            if (!type_expression) {
+                if (!type_base) {
+                    type_base = cType::InitializeType(gcDoUISetOrigin_base_name,
+                                                       gcDoUISetOrigin_base_desc,
+                                                       1, 0, 0, 0, 0, 0);
+                }
+                type_expression = cType::InitializeType(0, 0, 0x6A, type_base,
+                                                        0, 0, 0, 0);
+            }
+            type_action = cType::InitializeType(0, 0, 0x6B, type_expression,
+                                                0, 0, 0, 0);
+        }
+        type_gcDoUISetOrigin = cType::InitializeType(
+            0, 0, 0xE6, type_action, gcDoUISetOrigin::New, 0, 0, 0);
+    }
+    return type_gcDoUISetOrigin;
+}
 
 // 0x0030fca4, 160B
 cBase *gcDoUISetOrigin::New(cMemPool *pool, cBase *parent) {
@@ -114,4 +171,9 @@ void gcDoUISetOrigin::VisitReferences(unsigned int flags, cBase *ctx, void (*cb)
         cb(ctx, (unsigned int)(void *)this, user);
     }
     ((gcDesiredUIWidgetHelper *)((char *)this + 0xC))->VisitReferences(flags, (cBase *)this, cb, user, mask);
+}
+
+// 0x003101e0, 124B
+gcDoUISetOrigin::~gcDoUISetOrigin() {
+    *(void **)((char *)this + 4) = gcDoUISetOriginvirtualtable;
 }
