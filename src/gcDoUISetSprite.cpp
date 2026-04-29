df8e961d13a036a73145df53caca3e6a4282735e
autodecomp <autodecomp@localhost>
2026-04-29 18:25:10 -0500

    Match 2 functions (session 5d0b6222)
      - gcDoUIEffect::GetType
      - gcDoUISetSprite::GetType

--- src/gcDoUISetSprite.cpp
+++ src/gcDoUISetSprite.cpp
@@ -4,6 +4,15 @@
 class cBase;
 class cFile;
 class cMemPool;
+class cType;
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *,
+                                 cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
 
 class cWriteBlock {
 public:
@@ -52,10 +61,13 @@ extern "C" void gcDesiredUIWidgetHelper_gcDesiredUIWidgetHelper(void *, int);
 extern char gcDoUISetSpritevirtualtable[];
 extern char D_000006F8[];
 extern char D_003898A0[];
+extern const char gcDoUISetSprite_base_name[] asm("D_0036D894");
+extern const char gcDoUISetSprite_base_desc[] asm("D_0036D89C");
 
 class gcDoUISetSprite {
 public:
     static cBase *New(cMemPool *, cBase *);
+    const cType *GetType(void) const;
     void Write(cFile &) const;
 };
 
@@ -74,6 +86,11 @@ struct WriteSlot {
     short pad;
     void (*fn)(void *, cFile *);
 };
+
+static cType *type_action asm("D_000385D4");
+static cType *type_expression asm("D_000385D8");
+static cType *type_base asm("D_000385DC");
+static cType *type_gcDoUISetSprite asm("D_0009F740");
 
 // 0x00310560 - gcDoUISetSprite::New(cMemPool *, cBase *) static
 cBase *gcDoUISetSprite::New(cMemPool *pool, cBase *parent) {
@@ -98,6 +115,29 @@ cBase *gcDoUISetSprite::New(cMemPool *pool, cBase *parent) {
         result = obj;
     }
     return (cBase *)result;
+}
+
+// 0x00310628 - gcDoUISetSprite::GetType(void) const
+const cType *gcDoUISetSprite::GetType(void) const {
+    if (!type_gcDoUISetSprite) {
+        if (!type_action) {
+            if (!type_expression) {
+                if (!type_base) {
+                    type_base = cType::InitializeType(
+                        gcDoUISetSprite_base_name,
+                        gcDoUISetSprite_base_desc,
+                        1, 0, 0, 0, 0, 0);
+                }
+                type_expression = cType::InitializeType(
+                    0, 0, 0x6A, type_base, 0, 0, 0, 0);
+            }
+            type_action = cType::InitializeType(
+                0, 0, 0x6B, type_expression, 0, 0, 0, 0);
+        }
+        type_gcDoUISetSprite = cType::InitializeType(
+            0, 0, 0x16F, type_action, gcDoUISetSprite::New, 0, 0, 0);
+    }
+    return type_gcDoUISetSprite;
 }
 
 // 0x00310740 - gcDoUISetSprite::Write(cFile &) const
