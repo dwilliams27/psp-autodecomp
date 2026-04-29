f230ced5efd708c86bb1012e509929628cc34ae7
autodecomp <autodecomp@localhost>
2026-04-29 16:49:56 -0500

    Match 1 functions (session 9c3a77ed)
      - ePortal::GetType

--- include/ePortal.h
+++ include/ePortal.h
@@ -20,10 +20,13 @@ class ePortal : public cObject {
 public:
     ePortal(cBase *);
     ~ePortal(void);
+    const cType *GetType(void) const;
     void Activate(bool);
     void ConnectRoom(cHandleT<eRoom>, bool);
     void DisconnectRoom(cHandleT<eRoom>, bool);
     void VisitReferences(unsigned int, cBase *, void (*)(cBase *, unsigned int, void *), void *, unsigned int);
+    void Write(cFile &) const;
+    static cBase *New(cMemPool *, cBase *);
 };
 
 #endif
--- src/ePortal.cpp
+++ src/ePortal.cpp
@@ -1,16 +1,87 @@
 #include "ePortal.h"
 
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *, cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+class cNamed {
+public:
+    static cBase *New(cMemPool *, cBase *);
+};
+
+class cWriteBlock {
+public:
+    int _data[2];
+    cWriteBlock(cFile &, unsigned int);
+    void Write(float);
+    void Write(int, const float *);
+    void End(void);
+};
+
 extern char ePortalvirtualtable[];
 extern void *__vec_new(void *array, int count, int size, void (*ctor)(void *));
 extern void cHandleT_eRoom_ctor(void *);
 extern void cObject___dtor_cObject_void(void *, int);
 extern void *D_00038890[];
+extern cType *D_000385DC;
+extern cType *D_000385E0;
+extern cType *D_000385E4;
+extern cType *D_000469A0;
 
 struct DeleteRecord {
     short offset;
     short _pad;
     void (*fn)(void *, void *);
 };
+
+const cType *ePortal::GetType(void) const {
+    if (D_000469A0 == 0) {
+        if (D_000385E4 == 0) {
+            if (D_000385E0 == 0) {
+                if (D_000385DC == 0) {
+                    D_000385DC = cType::InitializeType((const char *)0x36CD74,
+                                                       (const char *)0x36CD7C,
+                                                       1, 0, 0, 0, 0, 0);
+                }
+                D_000385E0 = cType::InitializeType(0, 0, 2, D_000385DC,
+                                                   &cNamed::New, 0, 0, 0);
+            }
+            D_000385E4 = cType::InitializeType(0, 0, 3, D_000385E0,
+                                               0, 0, 0, 0);
+        }
+        D_000469A0 = cType::InitializeType(0, 0, 0x21E, D_000385E4,
+                                           &ePortal::New,
+                                           (const char *)0x36CE20,
+                                           (const char *)0x36CE28, 3);
+    }
+    return D_000469A0;
+}
+
+void ePortal::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    cObject::Write(file);
+
+    int i = 0;
+    const float *value = (const float *)((const char *)this + 0x50);
+    do {
+        wb.Write(3, value);
+        i++;
+        value += 4;
+    } while (i < 4);
+
+    wb.Write(3, (const float *)((const char *)this + 0x90));
+    wb.Write(*(const float *)((const char *)this + 0x9C));
+    wb.Write(3, (const float *)((const char *)this + 0xD0));
+    wb.Write(3, (const float *)((const char *)this + 0xA0));
+    wb.Write(3, (const float *)((const char *)this + 0xB0));
+    wb.Write(3, (const float *)((const char *)this + 0xC0));
+    wb.Write(*(const float *)((const char *)this + 0xE4));
+    wb.Write(*(const float *)((const char *)this + 0xE0));
+    wb.End();
+}
 
 void ePortal::Activate(bool active) {
     *(unsigned char *)((char *)this + 0xF0) = (unsigned char)active;
