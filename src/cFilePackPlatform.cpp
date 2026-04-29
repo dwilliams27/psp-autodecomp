e5e5c325c8e65a6d1075ec42fdb7670607d08a68
autodecomp <autodecomp@localhost>
2026-04-29 14:20:09 -0500

    Match 1 functions (session 1f9e2cbb)
      - cFilePackPlatform::PackClose

---
+++ src/cFilePackPlatform.cpp
@@ -0,0 +1,53 @@
+#include "cFileSystem.h"
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
+
+extern "C" void free(void *);
+
+struct DeleteRecord {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
+};
+
+class cFilePackPlatform {
+public:
+    void *mPackData;         // 0x00
+    char _pad4[0x14];        // 0x04
+    cFileHandle *mHandle;    // 0x18
+
+    ~cFilePackPlatform();
+    void PackClose(void);
+    void PackFree(void *);
+
+    static void operator delete(void *p) {
+        cMemPool *pool = cMemPool::GetPoolFromPtr(p);
+        if (pool != 0) {
+            char *block = ((char **)pool)[9];
+            DeleteRecord *rec = (DeleteRecord *)(((char **)block)[7] + 0x30);
+            short off = rec->offset;
+            void (*fn)(void *, void *) = rec->fn;
+            fn(block + off, p);
+        } else {
+            free(p);
+        }
+    }
+};
+
+void cFilePackPlatform::PackClose(void) {
+    if (mHandle != 0) {
+        cFileSystem::Close(mHandle);
+        mHandle = 0;
+    }
+}
+
+cFilePackPlatform::~cFilePackPlatform() {
+    if (mPackData != 0) {
+        PackFree(mPackData);
+        mPackData = 0;
+    }
+    PackClose();
+}
