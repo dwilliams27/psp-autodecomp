b3fe5f35480de68a8b639b45b0be9ee611f29ece
autodecomp <autodecomp@localhost>
2026-04-29 12:59:31 -0500

    Match 2 functions (session 63908180)
      - gcValCameraVariable::Write
      - gcValCameraVariable::~gcValCameraVariable

--- src/gcValCameraVariable.cpp
+++ src/gcValCameraVariable.cpp
@@ -1,6 +1,27 @@
 #include "cBase.h"
 
 class cMemPool;
+class cFile;
+
+class cWriteBlock {
+public:
+    cFile *file;
+    unsigned int _pos;
+    cWriteBlock(cFile &, unsigned int);
+    void Write(bool);
+    void Write(int);
+    void End(void);
+};
+
+class gcLValue {
+public:
+    void Write(cFile &) const;
+};
+
+class cMemPoolNS {
+public:
+    static cMemPoolNS *GetPoolFromPtr(const void *);
+};
 
 class gcDesiredCamera {
 public:
@@ -24,6 +45,12 @@ struct AllocEntry {
     void *(*fn)(void *, int, int, int, int);
 };
 
+struct DeleteRecord {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
+};
+
 class gcValCameraVariable {
 public:
     char _pad0[0x10];
@@ -34,7 +61,15 @@ public:
     int mField44;
 
     void AssignCopy(const cBase *);
+    void Write(cFile &) const;
     static cBase *New(cMemPool *, cBase *);
+    ~gcValCameraVariable(void);
+    static void operator delete(void *p) {
+        cMemPoolNS *pool = cMemPoolNS::GetPoolFromPtr(p);
+        char *block = ((char **)pool)[9];
+        DeleteRecord *rec = (DeleteRecord *)(((PoolBlock *)block)->allocTable + 0x30);
+        rec->fn(block + rec->offset, p);
+    }
 };
 
 template <class T> T *dcast(const cBase *);
@@ -42,6 +77,13 @@ template <class T> T *dcast(const cBase *);
 extern char cBaseclassdesc[];
 extern char gcValCameraVariablevirtualtable[];
 void gcDesiredCamera_gcDesiredCamera(void *, cBase *);
+extern "C" void gcDesiredCamera___dtor_gcDesiredCamera_void(void *, int);
+
+struct WriteRecord {
+    short offset;
+    short pad;
+    void (*fn)(void *, cFile *);
+};
 
 // 0x003223ac, 88B
 void gcValCameraVariable::AssignCopy(const cBase *base) {
@@ -72,4 +114,24 @@ cBase *gcValCameraVariable::New(cMemPool *pool, cBase *parent) {
         result = obj;
     }
     return (cBase *)result;
+}
+
+// 0x003225f8, 132B
+void gcValCameraVariable::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    ((const gcLValue *)this)->Write(file);
+    char *typeInfo = *(char **)((const char *)this + 12);
+    WriteRecord *rec = (WriteRecord *)(typeInfo + 0x28);
+    char *base = (char *)this + 8;
+    rec->fn(base + rec->offset, wb.file);
+    wb.Write(mField40);
+    wb.Write(mField44);
+    wb.End();
+}
+
+// 0x00322f2c, 136B
+gcValCameraVariable::~gcValCameraVariable(void) {
+    *(void **)((char *)this + 4) = gcValCameraVariablevirtualtable;
+    gcDesiredCamera___dtor_gcDesiredCamera_void((char *)this + 8, 2);
+    *(void **)((char *)this + 4) = cBaseclassdesc;
 }
