1f0cac1bd530512d5c0d7b16e95b5c8fce4b2fab
autodecomp <autodecomp@localhost>
2026-04-29 16:52:38 -0500

    Match 1 functions (session a75df635)
      - eProjectorTemplate::GetType

--- src/eProjectorTemplate.cpp
+++ src/eProjectorTemplate.cpp
@@ -1,8 +1,45 @@
 #include "cObject.h"
 
 class cBase;
+class cFile;
 class cMemPool;
+class cType;
 class eMaterial;
+
+class cNamed {
+public:
+    static cBase *New(cMemPool *, cBase *);
+};
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *,
+                                 cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+class cWriteBlock {
+public:
+    int _data[2];
+    cWriteBlock(cFile &, unsigned int);
+    void Write(bool);
+    void Write(int);
+    void Write(float);
+    void Write(int, const float *);
+    void End(void);
+};
+
+class eDynamicGeomTemplate {
+public:
+    void Write(cFile &) const;
+};
+
+class cHandle {
+public:
+    int mIndex;
+    void Write(cWriteBlock &) const;
+};
 
 inline void *operator new(unsigned int, void *p) {
     return p;
@@ -29,10 +66,19 @@ struct AllocRec {
     void *(*fn)(void *, int, int, int, int);
 };
 
+extern cType *D_000385DC;
+extern cType *D_000385E0;
+extern cType *D_000385E4;
+extern cType *D_000469A8;
+extern cType *D_000469E0;
+extern cType *D_00046C48;
+
 class eProjectorTemplate : public cObject {
 public:
     eProjectorTemplate(cBase *);
     void AssignCopy(const cBase *);
+    const cType *GetType(void) const;
+    void Write(cFile &) const;
     static cBase *New(cMemPool *, cBase *);
 };
 
@@ -90,5 +136,91 @@ cBase *eProjectorTemplate::New(cMemPool *pool, cBase *parent) {
         result = obj;
     }
     return (cBase *)result;
+}
+#pragma control sched=2
+
+// -- eProjectorTemplate::GetType(void) const @ 0x00214f18 --
+#pragma control sched=1
+const cType *eProjectorTemplate::GetType(void) const {
+    if (D_00046C48 == 0) {
+        if (D_000469E0 == 0) {
+            if (D_000469A8 == 0) {
+                if (D_000385E4 == 0) {
+                    if (D_000385E0 == 0) {
+                        if (D_000385DC == 0) {
+                            const char *name = (const char *)0x36CD74;
+                            const char *desc = (const char *)0x36CD7C;
+                            __asm__ volatile("" : "+r"(name), "+r"(desc));
+                            D_000385DC = cType::InitializeType(
+                                name, desc, 1, 0, 0, 0, 0, 0);
+                        }
+                        const cType *parentType = D_000385DC;
+                        cBase *(*factory)(cMemPool *, cBase *) = &cNamed::New;
+                        __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+                        D_000385E0 = cType::InitializeType(
+                            0, 0, 2, parentType, factory, 0, 0, 0);
+                    }
+                    D_000385E4 = cType::InitializeType(
+                        0, 0, 3, D_000385E0, 0, 0, 0, 0);
+                }
+                const cType *parentType = D_000385E4;
+                __asm__ volatile("" : "+r"(parentType));
+                __asm__ volatile("" ::: "memory");
+                const char *kindName = (const char *)0x36CE2C;
+                const char *kindDesc = (const char *)0x36CE3C;
+                __asm__ volatile("" : "+r"(kindName), "+r"(kindDesc));
+                D_000469A8 = cType::InitializeType(
+                    0, 0, 0x20, parentType, 0, kindName, kindDesc, 5);
+            }
+            D_000469E0 = cType::InitializeType(0, 0, 0x22, D_000469A8,
+                                               0, 0, 0, 0);
+        }
+        const cType *parentType = D_000469E0;
+        cBase *(*factory)(cMemPool *, cBase *) = &eProjectorTemplate::New;
+        __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+        D_00046C48 = cType::InitializeType(0, 0, 0x59, parentType, factory,
+                                           0, 0, 0);
+    }
+    return D_00046C48;
+}
+#pragma control sched=2
+
+// -- eProjectorTemplate::Write(cFile &) const @ 0x0007cf30 --
+#pragma control sched=1
+void eProjectorTemplate::Write(cFile &file) const {
+    cHandle *temp_s2;
+    int var_a0;
+    int var_s1;
+    int var_s2;
+    int var_s3;
+
+    cWriteBlock wb(file, 1);
+    ((const eDynamicGeomTemplate *)this)->Write(file);
+
+    temp_s2 = *(cHandle **)((char *)this + 0x48);
+    var_a0 = 0;
+    if (temp_s2 != 0) {
+        var_a0 = ((int *)temp_s2)[-1];
+        var_a0 &= 0x3FFFFFFF;
+    }
+    wb.Write(var_a0);
+
+    temp_s2 = *(cHandle **)((char *)this + 0x48);
+    var_s1 = 0;
+    if (temp_s2 != 0) {
+        var_s1 = ((int *)temp_s2)[-1];
+        var_s1 &= 0x3FFFFFFF;
+    }
+    cHandle *temp_a0 = temp_s2;
+    for (var_s2 = 0, var_s3 = 0; var_s2 < var_s1; var_s2++, var_s3 += 4) {
+        ((cHandle *)((char *)temp_a0 + var_s3))->Write(wb);
+    }
+
+    wb.Write(*(const bool *)((const char *)this + 0x4C));
+    wb.Write(*(const float *)((const char *)this + 0x50));
+    wb.Write(2, (const float *)((const char *)this + 0x5C));
+    wb.Write(*(const float *)((const char *)this + 0x54));
+    wb.Write(*(const float *)((const char *)this + 0x58));
+    wb.End();
 }
 #pragma control sched=2
