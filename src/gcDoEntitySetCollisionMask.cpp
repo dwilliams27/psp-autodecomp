85110ec68d80749e0499049c00ebe50ab78261ea
autodecomp <autodecomp@localhost>
2026-04-29 14:04:11 -0500

    Match 2 functions (session e0fab9bf)
      - gcDoEntitySetCollisionMask::Write
      - gcDoEntitySetCollisionMask::GetType

---
+++ src/gcDoEntitySetCollisionMask.cpp
@@ -0,0 +1,111 @@
+// gcDoEntitySetCollisionMask.cpp - decompiled from gcAll_psp.obj.
+//
+// Functions:
+//   0x002c5914 gcDoEntitySetCollisionMask::GetType(void) const   280B
+//   0x002c5a2c gcDoEntitySetCollisionMask::Write(cFile &) const  164B
+
+class cBase;
+class cFile;
+class cMemPool;
+class cType;
+
+class cWriteBlock {
+public:
+    cFile *mFile;
+    int mPos;
+
+    cWriteBlock(cFile &, unsigned int);
+    void Write(unsigned int);
+    void End(void);
+};
+
+class gcExpression {
+};
+
+class gcAction : public gcExpression {
+public:
+    cBase *mOwner;
+    void *mVTable;
+    unsigned int mNext;
+
+    void Write(cFile &) const;
+};
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *,
+                                 cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+typedef void (*gcDoEntitySetCollisionMaskWriteFn)(cBase *, cFile *);
+
+struct gcDoEntitySetCollisionMaskWriteSlot {
+    short mOffset;
+    short mPad;
+    gcDoEntitySetCollisionMaskWriteFn mWrite;
+};
+
+struct gcDoEntitySetCollisionMaskTypeInfo {
+    char mPad[0x28];
+    gcDoEntitySetCollisionMaskWriteSlot mSlot;
+};
+
+extern const char gcDoEntitySetCollisionMask_base_name[];
+extern const char gcDoEntitySetCollisionMask_base_desc[];
+
+class gcDoEntitySetCollisionMask : public gcAction {
+public:
+    static cBase *New(cMemPool *, cBase *);
+    const cType *GetType(void) const;
+    void Write(cFile &) const;
+};
+
+static cType *type_base;
+static cType *type_expression;
+static cType *type_action;
+static cType *type_gcDoEntitySetCollisionMask;
+
+const cType *gcDoEntitySetCollisionMask::GetType(void) const {
+    if (!type_gcDoEntitySetCollisionMask) {
+        if (!type_action) {
+            if (!type_expression) {
+                if (!type_base) {
+                    type_base = cType::InitializeType(
+                        gcDoEntitySetCollisionMask_base_name,
+                        gcDoEntitySetCollisionMask_base_desc,
+                        1, 0, 0, 0, 0, 0);
+                }
+                type_expression = cType::InitializeType(
+                    0, 0, 0x6A, type_base, 0, 0, 0, 0);
+            }
+            type_action = cType::InitializeType(
+                0, 0, 0x6B, type_expression, 0, 0, 0, 0);
+        }
+        type_gcDoEntitySetCollisionMask = cType::InitializeType(
+            0, 0, 0x14C, type_action, gcDoEntitySetCollisionMask::New, 0, 0, 0);
+    }
+    return type_gcDoEntitySetCollisionMask;
+}
+
+void gcDoEntitySetCollisionMask::Write(cFile &file) const {
+    cWriteBlock wb(file, 7);
+    gcAction::Write(file);
+    wb.Write(*(const unsigned int *)((const char *)this + 0xC));
+    wb.Write(*(const unsigned int *)((const char *)this + 0x10));
+
+    gcDoEntitySetCollisionMaskTypeInfo *type0 =
+        *(gcDoEntitySetCollisionMaskTypeInfo **)((const char *)this + 0x18);
+    gcDoEntitySetCollisionMaskWriteSlot *slot0 = &type0->mSlot;
+    cBase *entity0 = (cBase *)((char *)this + 0x14);
+    slot0->mWrite((cBase *)((char *)entity0 + slot0->mOffset), wb.mFile);
+
+    gcDoEntitySetCollisionMaskTypeInfo *type1 =
+        *(gcDoEntitySetCollisionMaskTypeInfo **)((const char *)this + 0x44);
+    gcDoEntitySetCollisionMaskWriteSlot *slot1 = &type1->mSlot;
+    cBase *entity1 = (cBase *)((char *)this + 0x40);
+    slot1->mWrite((cBase *)((char *)entity1 + slot1->mOffset), wb.mFile);
+
+    wb.End();
+}
