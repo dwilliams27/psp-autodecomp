ebc9db278661645e55726d0c802ca110d14803ae
autodecomp <autodecomp@localhost>
2026-04-29 14:08:34 -0500

    Match 1 functions (session bac98164)
      - eSprite::Write

--- src/eSprite.cpp
+++ src/eSprite.cpp
@@ -1,11 +1,35 @@
 // eSprite.cpp -- decompiled from eAll_psp.obj.
 //
 // Functions:
+//   0x00038230 eSprite::Write(cFile &) const
+//   0x00038ce8 eSprite::SetTexCoord1(mVec2)
 //   0x001e6f84 eSprite::New(cMemPool *, cBase *) static
 //   0x001e7428 eSprite::~eSprite(void)
 
 class cBase;
+class cFile;
 class cMemPool;
+struct mVec2 {
+    float x;
+    float y;
+};
+
+class cWriteBlock {
+public:
+    int _data[2];
+
+    cWriteBlock(cFile &, unsigned int);
+    void Write(int);
+    void Write(int, const float *);
+    void End(void);
+};
+
+class cHandle {
+public:
+    int _data;
+
+    void Write(cWriteBlock &) const;
+};
 
 inline void *operator new(unsigned int, void *p) {
     return p;
@@ -35,24 +59,28 @@ public:
     cObject(cBase *);
     ~cObject();
     cObject &operator=(const cObject &);
+    void Write(cFile &) const;
 };
 
 class eSprite : public cObject {
 public:
     int mField44;
-    int mField48;
+    cHandle mField48;
     float mField4C;
     float mField50;
     float mField54;
     float mField58;
-    int mField5C;
-    int mField60;
-    int mField64;
-    int mField68;
+    float mField5C;
+    float mField60;
+    float mField64;
+    float mField68;
 
     eSprite(cBase *);
     ~eSprite();
     void AssignCopy(const cBase *);
+    void Write(cFile &) const;
+    void SetTexCoord1(mVec2);
+    bool GetTextureSize(mVec2 *) const;
     static cBase *New(cMemPool *, cBase *);
 
     static void operator delete(void *p) {
@@ -70,6 +98,29 @@ template <class T> T *dcast(const cBase *);
 struct CopyWord {
     int value;
 };
+
+// -- eSprite::Write(cFile &) const @ 0x00038230 --
+void eSprite::Write(cFile &file) const {
+    cWriteBlock wb(file, 4);
+    ((const cObject *)this)->Write(file);
+    wb.Write(mField44);
+    wb.Write(2, &mField4C);
+    mField48.Write(wb);
+    wb.Write(2, &mField54);
+    wb.Write(2, &mField5C);
+    wb.Write(2, &mField64);
+    wb.End();
+}
+
+// -- eSprite::SetTexCoord1(mVec2) @ 0x00038ce8 --
+void eSprite::SetTexCoord1(mVec2 texCoord) {
+    mVec2 textureSize;
+
+    if (GetTextureSize(&textureSize)) {
+        mField54 = texCoord.x / textureSize.x;
+        mField58 = texCoord.y / textureSize.y;
+    }
+}
 
 // -- eSprite::AssignCopy(const cBase *) @ 0x001e6eec --
 void eSprite::AssignCopy(const cBase *src) {
