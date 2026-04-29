2c165e6926ab5447fdb760329677ea24daa111c6
autodecomp <autodecomp@localhost>
2026-04-29 17:05:53 -0500

    Match 2 functions (session bc0ea690)
      - eStandardHeightmapMtl::GetType
      - eStandardParticleSystemMtl::GetType

--- src/eStandardHeightmapMtl.cpp
+++ src/eStandardHeightmapMtl.cpp
@@ -2,6 +2,23 @@
 #include "eStandardParticleSystemMtl.h"
 #include "eStandardWeatherEffectMtl.h"
 
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *, cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+extern cType *D_000385DC;
+extern cType *D_000385E0;
+extern cType *D_000385E4;
+extern cType *D_00040FEC;
+extern cType *D_00040FF8;
+extern cType *D_0004104C;
+extern cType *D_00046C38;
+extern cType *D_00046CAC;
+extern cType *D_00046CB0;
+
 void eStandardHeightmapMtl::CreateData(void) {
 }
 
@@ -53,6 +70,102 @@ public:
 void cFile_SetCurrentPos(void *, unsigned int);
 
 #pragma control sched=1
+
+// ── GetType ──
+
+const cType *eStandardHeightmapMtl::GetType(void) const {
+    if (D_00046CAC == 0) {
+        if (D_00046C38 == 0) {
+            if (D_00040FF8 == 0) {
+                if (D_00040FEC == 0) {
+                    if (D_000385E4 == 0) {
+                        if (D_000385E0 == 0) {
+                            if (D_000385DC == 0) {
+                                const char *name = (const char *)0x36CD74;
+                                const char *desc = (const char *)0x36CD7C;
+                                __asm__ volatile("" : "+r"(name), "+r"(desc));
+                                D_000385DC = cType::InitializeType(
+                                    name, desc, 1, 0, 0, 0, 0, 0);
+                            }
+                            const cType *parentType = D_000385DC;
+                            cBase *(*factory)(cMemPool *, cBase *) =
+                                (cBase *(*)(cMemPool *, cBase *))0x1C3C58;
+                            __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+                            D_000385E0 = cType::InitializeType(
+                                0, 0, 2, parentType, factory, 0, 0, 0);
+                        }
+                        D_000385E4 = cType::InitializeType(
+                            0, 0, 3, D_000385E0, 0, 0, 0, 0);
+                    }
+                    const cType *parentType = D_000385E4;
+                    const char *kindName = (const char *)0x36CDCC;
+                    const char *kindDesc = (const char *)0x36CDD8;
+                    __asm__ volatile("" : "+r"(parentType), "+r"(kindName), "+r"(kindDesc));
+                    D_00040FEC = cType::InitializeType(
+                        0, 0, 0x10, parentType, 0, kindName, kindDesc, 5);
+                }
+                D_00040FF8 = cType::InitializeType(0, 0, 0x12, D_00040FEC,
+                                                   0, 0, 0, 0);
+            }
+            D_00046C38 = cType::InitializeType(0, 0, 0x51, D_00040FF8,
+                                               0, 0, 0, 0);
+        }
+        const cType *parentType = D_00046C38;
+        cBase *(*factory)(cMemPool *, cBase *) =
+            (cBase *(*)(cMemPool *, cBase *))&eStandardHeightmapMtl::New;
+        __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+        D_00046CAC = cType::InitializeType(0, 0, 0x52, parentType, factory,
+                                           0, 0, 0);
+    }
+    return D_00046CAC;
+}
+
+const cType *eStandardParticleSystemMtl::GetType(void) const {
+    if (D_00046CB0 == 0) {
+        if (D_0004104C == 0) {
+            if (D_00040FF8 == 0) {
+                if (D_00040FEC == 0) {
+                    if (D_000385E4 == 0) {
+                        if (D_000385E0 == 0) {
+                            if (D_000385DC == 0) {
+                                const char *name = (const char *)0x36CD74;
+                                const char *desc = (const char *)0x36CD7C;
+                                __asm__ volatile("" : "+r"(name), "+r"(desc));
+                                D_000385DC = cType::InitializeType(
+                                    name, desc, 1, 0, 0, 0, 0, 0);
+                            }
+                            const cType *parentType = D_000385DC;
+                            cBase *(*factory)(cMemPool *, cBase *) =
+                                (cBase *(*)(cMemPool *, cBase *))0x1C3C58;
+                            __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+                            D_000385E0 = cType::InitializeType(
+                                0, 0, 2, parentType, factory, 0, 0, 0);
+                        }
+                        D_000385E4 = cType::InitializeType(
+                            0, 0, 3, D_000385E0, 0, 0, 0, 0);
+                    }
+                    const cType *parentType = D_000385E4;
+                    const char *kindName = (const char *)0x36CDCC;
+                    const char *kindDesc = (const char *)0x36CDD8;
+                    __asm__ volatile("" : "+r"(parentType), "+r"(kindName), "+r"(kindDesc));
+                    D_00040FEC = cType::InitializeType(
+                        0, 0, 0x10, parentType, 0, kindName, kindDesc, 5);
+                }
+                D_00040FF8 = cType::InitializeType(0, 0, 0x12, D_00040FEC,
+                                                   0, 0, 0, 0);
+            }
+            D_0004104C = cType::InitializeType(0, 0, 0x32, D_00040FF8,
+                                               0, 0, 0, 0);
+        }
+        const cType *parentType = D_0004104C;
+        cBase *(*factory)(cMemPool *, cBase *) =
+            (cBase *(*)(cMemPool *, cBase *))&eStandardParticleSystemMtl::New;
+        __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+        D_00046CB0 = cType::InitializeType(0, 0, 0x33, parentType, factory,
+                                           0, 0, 0);
+    }
+    return D_00046CB0;
+}
 
 // ── Write ──
 
