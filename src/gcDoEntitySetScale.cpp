3cc8aea7ee3275d042c2437f2a363ae63bba630a
autodecomp <autodecomp@localhost>
2026-04-29 18:07:31 -0500

    Match 2 functions (session a44ce3f9)
      - gcDoEntitySetScale::GetType
      - gcDoEntitySetScale::New

--- src/gcDoEntitySetScale.cpp
+++ src/gcDoEntitySetScale.cpp
@@ -1,7 +1,6 @@
-// gcDoEntitySetScale.cpp - gcAll_psp.obj
-
 class cBase;
 class cFile;
+class cMemPool;
 
 class cWriteBlock {
 public:
@@ -11,6 +10,20 @@ public:
     cWriteBlock(cFile &, unsigned int);
     void Write(int);
     void End(void);
+};
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *,
+                                 cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+class gcDesiredValue {
+public:
+    int mField0;
+    void Write(cWriteBlock &) const;
 };
 
 class gcExpression {
@@ -25,10 +38,15 @@ public:
     void Write(cFile &) const;
 };
 
-class gcDesiredValue {
-public:
-    int mField0;
-    void Write(cWriteBlock &) const;
+struct PoolBlock {
+    char pad[0x1C];
+    char *allocTable;
+};
+
+struct AllocEntry {
+    short offset;
+    short pad;
+    void *(*fn)(void *, int, int, int, int);
 };
 
 struct WriteRec {
@@ -37,14 +55,88 @@ struct WriteRec {
     void (*fn)(void *, cFile *);
 };
 
+extern "C" void gcAction_gcAction(void *, cBase *);
+extern "C" void gcDesiredObject_gcDesiredObject(void *, cBase *);
+extern "C" void gcDesiredEntityHelper_ctor(void *, int, int, int)
+    __asm__("gcDesiredEntityHelper__gcDesiredEntityHelper_gcDesiredEntityHelper__gcPrimary_gcDesiredEntityHelper__gcRelationship_gcDesiredEntityHelper__gcRelationship__0011B714");
+
+extern char D_00000338[];
+extern char gcDoEntitySetScalevirtualtable[];
+
 class gcDoEntitySetScale : public gcAction {
 public:
+    static cBase *New(cMemPool *, cBase *);
+    const cType *GetType(void) const;
+    void Write(cFile &) const;
+
     char _pad0C[0x2C];        // 0x0C
     gcDesiredValue mDesired;  // 0x38
     int mScaleType;           // 0x3C
-
-    void Write(cFile &) const;
 };
+
+static cType *type_base asm("D_000385DC");
+static cType *type_expression asm("D_000385D8");
+static cType *type_action asm("D_000385D4");
+static cType *type_gcDoEntitySetScale asm("D_0009F660");
+
+// 0x002cbb14 - gcDoEntitySetScale::GetType(void) const
+const cType *gcDoEntitySetScale::GetType(void) const {
+    if (!type_gcDoEntitySetScale) {
+        if (!type_action) {
+            if (!type_expression) {
+                if (!type_base) {
+                    type_base = cType::InitializeType((const char *)0x36D894,
+                                                      (const char *)0x36D89C,
+                                                      1, 0, 0, 0, 0, 0);
+                }
+                type_expression = cType::InitializeType(0, 0, 0x6A,
+                                                        type_base, 0, 0, 0, 0);
+            }
+            type_action = cType::InitializeType(0, 0, 0x6B, type_expression,
+                                                0, 0, 0, 0);
+        }
+        type_gcDoEntitySetScale = cType::InitializeType(
+            0, 0, 0xCD, type_action, gcDoEntitySetScale::New, 0, 0, 0);
+    }
+    return type_gcDoEntitySetScale;
+}
+
+// 0x002cba0c - gcDoEntitySetScale::New(cMemPool *, cBase *) static
+cBase *gcDoEntitySetScale::New(cMemPool *pool, cBase *parent) {
+    void *block = ((void **)pool)[9];
+    char *allocTable = ((PoolBlock *)block)->allocTable;
+    AllocEntry *entry = (AllocEntry *)(allocTable + 0x28);
+    short off = entry->offset;
+    void *base = (char *)block + off;
+    gcDoEntitySetScale *result = 0;
+    gcDoEntitySetScale *obj =
+        (gcDoEntitySetScale *)entry->fn(base, 0x40, 4, 0, 0);
+    if (obj != 0) {
+        gcAction_gcAction(obj, parent);
+        ((void **)obj)[1] = gcDoEntitySetScalevirtualtable;
+
+        char *desired = (char *)obj + 0x0C;
+        gcDesiredObject_gcDesiredObject(desired, (cBase *)obj);
+        ((void **)obj)[4] = D_00000338;
+
+        char *helper = (char *)obj + 0x18;
+        int one = 1;
+        gcDesiredEntityHelper_ctor(helper, 1, 0, 0);
+        ((void **)obj)[4] = (void *)0x388A48;
+        ((void **)obj)[9] = (void *)0x37E6A8;
+        ((void **)obj)[8] = desired;
+        ((void **)obj)[9] = (void *)0x388568;
+        *(unsigned char *)((char *)obj + 0x28) = one;
+        *(unsigned char *)((char *)obj + 0x29) = 0;
+        *(int *)((char *)obj + 0x2C) = 0;
+        *(int *)((char *)obj + 0x30) = 0;
+        *(int *)((char *)obj + 0x34) = (int)desired | 1;
+        *(int *)((char *)obj + 0x38) = (int)obj | 1;
+        *(int *)((char *)obj + 0x3C) = 0;
+        result = obj;
+    }
+    return (cBase *)result;
+}
 
 // 0x002cbc2c - gcDoEntitySetScale::Write(cFile &) const
 void gcDoEntitySetScale::Write(cFile &file) const {
