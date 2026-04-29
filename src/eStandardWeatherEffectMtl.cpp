84cb1cde6df5f96db768f7626995b4b7ccb00cc5
autodecomp <autodecomp@localhost>
2026-04-29 17:08:28 -0500

    Match 2 functions (session 57ecb7ed)
      - eStandardWeatherEffectMtl::GetType
      - eStandardWeatherEffectMtl::eStandardWeatherEffectMtl

--- src/eStandardWeatherEffectMtl.cpp
+++ src/eStandardWeatherEffectMtl.cpp
@@ -2,6 +2,21 @@
 #include "eTwoPassModelMtl.h"
 #include "eTextureMap.h"
 
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *, cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+extern cType *D_000385DC;
+extern cType *D_000385E0;
+extern cType *D_000385E4;
+extern cType *D_00040FEC;
+extern cType *D_00040FF8;
+extern cType *D_00046BA4;
+extern cType *D_00046CB4;
+
 class cWriteBlock {
 public:
     int _data[2];
@@ -36,6 +51,66 @@ public:
 void cFile_SetCurrentPos(void *, unsigned int);
 
 #pragma control sched=1
+
+const cType *eStandardWeatherEffectMtl::GetType(void) const {
+    if (D_00046CB4 == 0) {
+        if (D_00046BA4 == 0) {
+            if (D_00040FF8 == 0) {
+                if (D_00040FEC == 0) {
+                    if (D_000385E4 == 0) {
+                        if (D_000385E0 == 0) {
+                            if (D_000385DC == 0) {
+                                const char *name = (const char *)0x36CD74;
+                                const char *desc = (const char *)0x36CD7C;
+                                __asm__ volatile("" : "+r"(name), "+r"(desc));
+                                D_000385DC = cType::InitializeType(
+                                    name, desc, 1, 0, 0, 0, 0, 0);
+                            }
+                            const cType *parentType = D_000385DC;
+                            cBase *(*factory)(cMemPool *, cBase *) =
+                                (cBase *(*)(cMemPool *, cBase *))0x1C3C58;
+                            __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+                            D_000385E0 = cType::InitializeType(
+                                0, 0, 2, parentType, factory, 0, 0, 0);
+                        }
+                        D_000385E4 = cType::InitializeType(
+                            0, 0, 3, D_000385E0, 0, 0, 0, 0);
+                    }
+                    const cType *parentType = D_000385E4;
+                    const char *kindName = (const char *)0x36CDCC;
+                    const char *kindDesc = (const char *)0x36CDD8;
+                    __asm__ volatile("" : "+r"(parentType), "+r"(kindName), "+r"(kindDesc));
+                    D_00040FEC = cType::InitializeType(
+                        0, 0, 0x10, parentType, 0, kindName, kindDesc, 5);
+                }
+                D_00040FF8 = cType::InitializeType(0, 0, 0x12, D_00040FEC,
+                                                   0, 0, 0, 0);
+            }
+            D_00046BA4 = cType::InitializeType(0, 0, 0x193, D_00040FF8,
+                                               0, 0, 0, 0);
+        }
+        const cType *parentType = D_00046BA4;
+        cBase *(*factory)(cMemPool *, cBase *) =
+            (cBase *(*)(cMemPool *, cBase *))&eStandardWeatherEffectMtl::New;
+        __asm__ volatile("" : "+r"(parentType), "+r"(factory));
+        D_00046CB4 = cType::InitializeType(0, 0, 0x194, parentType, factory,
+                                           0, 0, 0);
+    }
+    return D_00046CB4;
+}
+
+extern "C" void eGeomMtl_eGeomMtl(void *, cBase *);
+
+extern char eStandardWeatherEffectMtlclassdesc[];
+
+eStandardWeatherEffectMtl::eStandardWeatherEffectMtl(cBase *parent) {
+    eGeomMtl_eGeomMtl(this, parent);
+    ((int *)this)[0x68 / 4] = -1;
+    ((void **)this)[0x04 / 4] = eStandardWeatherEffectMtlclassdesc;
+    __asm__ volatile("" ::: "memory");
+    ((unsigned char *)this)[0x5C] |= 8;
+    CreateData();
+}
 
 void eStandardWeatherEffectMtl::Unapply(void) const {
 }
