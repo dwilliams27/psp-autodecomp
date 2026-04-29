9c43c2408821869d63a4ed9e55bb91a027fa31b4
autodecomp <autodecomp@localhost>
2026-04-29 13:41:00 -0500

    Match 2 functions (session 60c9ee8a)
      - gcDoNetworkOp::Write
      - gcDoNetworkOp::GetType

--- src/gcDoNetworkOp.cpp
+++ src/gcDoNetworkOp.cpp
@@ -1,5 +1,29 @@
 class cBase;
+class cFile;
 class cMemPool;
+class cType;
+class gcDoNetworkOp;
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *, cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+class cWriteBlock {
+public:
+    int _data[2];
+    cWriteBlock(cFile &, unsigned int);
+    void Write(int);
+    void WriteBase(const cBase *);
+    void End(void);
+};
+
+class gcDesiredValue {
+public:
+    void Write(cWriteBlock &) const;
+};
 
 struct PoolBlock {
     char pad[0x1C];
@@ -13,12 +37,22 @@ struct AllocEntry {
 };
 
 void gcAction_gcAction(void *, cBase *);
+void gcAction_Write(const gcDoNetworkOp *, cFile &);
+extern const char gcDoNetworkOp_base_name[];
+extern const char gcDoNetworkOp_base_desc[];
 extern char gcDoNetworkOpvirtualtable[];
 
 class gcDoNetworkOp {
 public:
     static cBase *New(cMemPool *, cBase *);
+    void Write(cFile &) const;
+    const cType *GetType(void) const;
 };
+
+static cType *type_base;
+static cType *type_expression;
+static cType *type_action;
+static cType *type_gcDoNetworkOp;
 
 cBase *gcDoNetworkOp::New(cMemPool *pool, cBase *parent) {
     void *block = ((void **)pool)[9];
@@ -37,4 +71,46 @@ cBase *gcDoNetworkOp::New(cMemPool *pool, cBase *parent) {
         result = obj;
     }
     return (cBase *)result;
+}
+
+void gcDoNetworkOp::Write(cFile &file) const {
+    cWriteBlock wb(file, 3);
+    gcAction_Write(this, file);
+    wb.Write(((int *)this)[3]);
+    ((gcDesiredValue *)((char *)this + 0x10))->Write(wb);
+
+    int val = ((int *)this)[5];
+    int flag = 0;
+    if (val & 1) {
+        flag = 1;
+    }
+    cBase *ptr;
+    if (flag != 0) {
+        ptr = 0;
+    } else {
+        ptr = (cBase *)val;
+    }
+    wb.WriteBase(ptr);
+    wb.End();
+}
+
+const cType *gcDoNetworkOp::GetType(void) const {
+    if (!type_gcDoNetworkOp) {
+        if (!type_action) {
+            if (!type_expression) {
+                if (!type_base) {
+                    type_base = cType::InitializeType(gcDoNetworkOp_base_name,
+                                                      gcDoNetworkOp_base_desc,
+                                                      1, 0, 0, 0, 0, 0);
+                }
+                type_expression = cType::InitializeType(0, 0, 0x6A, type_base,
+                                                        0, 0, 0, 0);
+            }
+            type_action = cType::InitializeType(0, 0, 0x6B, type_expression,
+                                                0, 0, 0, 0);
+        }
+        type_gcDoNetworkOp = cType::InitializeType(0, 0, 0x1A8, type_action,
+                                                   gcDoNetworkOp::New, 0, 0, 0);
+    }
+    return type_gcDoNetworkOp;
 }
