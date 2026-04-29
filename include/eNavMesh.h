e5d0ea166fe97a6c4ca3ff12b59b9ac5551dc358
autodecomp <autodecomp@localhost>
2026-04-29 16:42:04 -0500

    Match 1 functions (session 664a381c)
      - eNavMesh::GetType

--- include/eNavMesh.h
+++ include/eNavMesh.h
@@ -1,13 +1,18 @@
 #ifndef ENAVMESH_H
 #define ENAVMESH_H
 
+#include "cObject.h"
+
 class cMemPool;
 class cBase;
+class cType;
 struct mVec3;
 struct eNavMeshPos;
 
-class eNavMesh {
+class eNavMesh : public cObject {
 public:
+    eNavMesh(cBase *);
+    const cType *GetType(void) const;
     static cBase *New(cMemPool *, cBase *);
     static eNavMesh *FindNavMeshFromPoint(const mVec3 *, unsigned char &);
     static int FindNavMeshFromPoint(eNavMeshPos *);
--- src/eNavMesh.cpp
+++ src/eNavMesh.cpp
@@ -3,6 +3,18 @@
 
 #pragma control sched=1
 
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *, cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+class cNamed {
+public:
+    static cBase *New(cMemPool *, cBase *);
+};
+
 template <class T> T *dcast(const cBase *);
 
 template <class T>
@@ -28,6 +40,71 @@ struct PoolBlock {
     char pad[0x1C];
     char *allocTable;
 };
+
+extern cType *D_000385DC;
+extern cType *D_000385E0;
+extern cType *D_000385E4;
+extern cType *D_00046A30;
+extern int D_00037C128;
+extern int D_0001AF48[];
+
+// -- eNavMesh::GetType(void) const @ 0x00203244 --
+const cType *eNavMesh::GetType(void) const {
+    if (D_00046A30 == 0) {
+        if (D_000385E4 == 0) {
+            if (D_000385E0 == 0) {
+                if (D_000385DC == 0) {
+                    const char *baseName = (const char *)0x36CD74;
+                    const char *baseDesc = (const char *)0x36CD7C;
+                    __asm__ volatile("" : "+r"(baseName), "+r"(baseDesc));
+                    D_000385DC = cType::InitializeType(baseName, baseDesc,
+                                                       1, 0, 0, 0, 0, 0);
+                }
+                const cType *parentType = D_000385DC;
+                cBase *(*namedFactory)(cMemPool *, cBase *) = &cNamed::New;
+                __asm__ volatile("" : "+r"(parentType), "+r"(namedFactory));
+                D_000385E0 = cType::InitializeType(0, 0, 2, parentType,
+                                                   namedFactory, 0, 0, 0);
+            }
+            D_000385E4 = cType::InitializeType(0, 0, 3, D_000385E0,
+                                               0, 0, 0, 0);
+        }
+        const cType *parentType = D_000385E4;
+        cBase *(*factory)(cMemPool *, cBase *) = &eNavMesh::New;
+        const char *kindName = (const char *)0x36CE94;
+        const char *kindDesc = (const char *)0x36CEA0;
+        __asm__ volatile("" : "+r"(parentType), "+r"(factory),
+                         "+r"(kindName), "+r"(kindDesc));
+        D_00046A30 = cType::InitializeType(0, 0, 0xC4, parentType,
+                                           factory, kindName, kindDesc, 4);
+    }
+    return D_00046A30;
+}
+
+#pragma control sched=2
+
+// -- eNavMesh::eNavMesh(cBase *) @ 0x00057704 --
+eNavMesh::eNavMesh(cBase *parent) : cObject(parent) {
+    *(void **)((char *)this + 4) = (void *)0x381ED0;
+    *(int *)((char *)this + 0x44) = 0;
+    *(int *)((char *)this + 0x48) = 0;
+    *(int *)((char *)this + 0x4C) = 0;
+    *(int *)((char *)this + 0x80) = 0;
+    *(unsigned char *)((char *)this + 0x84) = 0;
+    int i = 0;
+    int *p;
+    if (D_00037C128 < 0) {
+        p = D_0001AF48;
+        do {
+            *p = 0;
+            i += 1;
+            p += 1;
+        } while (i < 64);
+        D_00037C128 = 0;
+    }
+}
+
+#pragma control sched=1
 
 // -- eNavMesh::AssignCopy(const cBase *) @ 0x00203138 --
 void eNavMesh::AssignCopy(const cBase *base) {
