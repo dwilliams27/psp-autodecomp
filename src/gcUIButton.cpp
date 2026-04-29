47f70033634a663bc3b56a26d20d52aa5b3af112
autodecomp <autodecomp@localhost>
2026-04-29 13:07:23 -0500

    Match 1 functions (session a8fe1354)
      - gcUIButton::IsUpdateEmpty

--- src/gcUIButton.cpp
+++ src/gcUIButton.cpp
@@ -2,8 +2,11 @@
 // Functions:
 //   0x0013984c  ~gcUIButton(void)
 //   0x0028e558  New(cMemPool *, cBase *) static
+//   0x00139654  Write(cFile &) const
+//   0x00139a28  IsUpdateEmpty(bool, bool) const
 
 class cBase;
+class cFile;
 class cMemPool;
 
 inline void *operator new(unsigned int, void *p) {
@@ -27,16 +30,38 @@ public:
     static cMemPoolHelper *GetPoolFromPtr(const void *);
 };
 
-class gcUITextControl {
+class cWriteBlock {
+public:
+    int _data[2];
+    cWriteBlock(cFile &, unsigned int);
+    void Write(int);
+    void End(void);
+};
+
+class cHandle {
+public:
+    void Write(cWriteBlock &) const;
+};
+
+class gcUIWidget {
+public:
+    int IsUpdateEmpty(bool, bool) const;
+};
+
+class gcUITextControl : public gcUIWidget {
 public:
     gcUITextControl(cBase *);
     ~gcUITextControl();
+    int IsUpdateEmpty(bool, bool) const;
+    void Write(cFile &) const;
 };
 
 class gcUIButton : public gcUITextControl {
 public:
     gcUIButton(cBase *);
     ~gcUIButton();
+    void Write(cFile &) const;
+    int IsUpdateEmpty(bool, bool) const;
 
     static cBase *New(cMemPool *, cBase *);
 
@@ -54,6 +79,35 @@ extern char gcUIButtonvirtualtable[];
 
 gcUIButton::~gcUIButton() {
     *(void **)((char *)this + 4) = gcUIButtonvirtualtable;
+}
+
+// -- gcUIButton::Write(cFile &) const @ 0x00139654 --
+void gcUIButton::Write(cFile &file) const {
+    cWriteBlock wb(file, 1);
+    ((const gcUITextControl *)this)->Write(file);
+    wb.Write(2);
+
+    int i = 0;
+    cHandle *handle = (cHandle *)((char *)this + 0x114);
+    do {
+        handle->Write(wb);
+        i += 1;
+        handle = (cHandle *)((char *)handle + 4);
+    } while (i < 2);
+
+    wb.End();
+}
+
+// -- gcUIButton::IsUpdateEmpty(bool, bool) const @ 0x00139a28 --
+int gcUIButton::IsUpdateEmpty(bool a, bool b) const {
+    if (a) {
+        int flag = (*(unsigned int *)((const char *)this + 0x24) & 8) != 0;
+        flag &= 0xFF;
+        if (flag) {
+            return 0;
+        }
+    }
+    return ((const gcUIWidget *)this)->IsUpdateEmpty(a, b);
 }
 
 // -- gcUIButton::New(cMemPool *, cBase *) static @ 0x0028e558 --
