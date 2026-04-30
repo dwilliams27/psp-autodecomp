4fcb43d11b0792f96f36aa3fc71fbdc813deb4f3
autodecomp <autodecomp@localhost>
2026-04-30 02:11:13 -0500

    Match 1 functions (session 90183265)
      - gcDesiredEntityTemplate::HasCategory

--- src/gcDesiredEntityTemplate.cpp
+++ src/gcDesiredEntityTemplate.cpp
@@ -3,6 +3,14 @@ class cFile;
 class cMemPool;
 class cObject;
 class cType;
+class gcEnumeration;
+class gcEnumerationEntry;
+
+template <class T>
+class cSubHandleT;
+
+template <class T, class U>
+class cHandlePairT;
 
 class cWriteBlock {
 public:
@@ -18,6 +26,11 @@ public:
     void Write(cWriteBlock &) const;
 };
 
+class gcEntityTemplate {
+public:
+    int HasCategory(const cHandlePairT<gcEnumeration, cSubHandleT<gcEnumerationEntry> > &) const;
+};
+
 class gcDesiredObject {
 public:
     void Write(cFile &) const;
@@ -25,6 +38,9 @@ public:
 
 class cType {
 public:
+    char _pad[0x1C];
+    const cType *mParent;
+
     static cType *InitializeType(const char *, const char *, unsigned int,
                                  const cType *,
                                  cBase *(*)(cMemPool *, cBase *),
@@ -35,11 +51,20 @@ extern cType *D_000385DC;
 extern cType *D_0009F3F4;
 extern cType *D_0009F44C;
 
+struct DispatchEntry {
+    short offset;
+    short _pad;
+    cType *(*fn)(void *, short, void *);
+};
+
 class gcDesiredEntityTemplate {
 public:
     cObject *Get(bool) const;
     cObject *GetObject(bool) const;
+    int HasCategory(const cHandlePairT<gcEnumeration, cSubHandleT<gcEnumerationEntry> > &) const;
     void Write(cFile &) const;
+    void AssignCopy(const cBase *);
+    gcDesiredEntityTemplate &operator=(const gcDesiredEntityTemplate &);
     static cBase *New(cMemPool *, cBase *);
     const cType *GetType(void) const;
 };
@@ -88,4 +113,73 @@ const cType *gcDesiredEntityTemplate::GetType(void) const {
                                            0, 0, 0);
     }
     return D_0009F44C;
+}
+
+void gcDesiredEntityTemplate::AssignCopy(const cBase *base) {
+    const gcDesiredEntityTemplate *other = 0;
+    const cBase *src = base;
+    gcDesiredEntityTemplate *self = this;
+
+    if (base != 0) {
+        if (D_0009F44C == 0) {
+            if (D_0009F3F4 == 0) {
+                if (D_000385DC == 0) {
+                    D_000385DC = cType::InitializeType((const char *)0x36D894,
+                                                       (const char *)0x36D89C,
+                                                       1, 0, 0, 0, 0, 0);
+                }
+                D_0009F3F4 = cType::InitializeType(0, 0, 0x12C, D_000385DC,
+                                                   0, 0, 0, 0);
+            }
+            D_0009F44C = cType::InitializeType(0, 0, 0x1A6, D_0009F3F4,
+                                               &gcDesiredEntityTemplate::New,
+                                               0, 0, 0);
+        }
+
+        void *classDesc = *(void **)((char *)src + 4);
+        cType *target = D_0009F44C;
+        DispatchEntry *entry = (DispatchEntry *)((char *)classDesc + 8);
+        short offset = entry->offset;
+        cType *(*fn)(void *, short, void *) = entry->fn;
+        cType *type = fn((char *)src + offset, offset, fn);
+        int isValid;
+
+        if (target != 0) {
+            goto have_target;
+        }
+        isValid = 0;
+        goto cast_done;
+
+have_target:
+        if (type != 0) {
+loop_cast:
+            if (type == target) {
+                isValid = 1;
+            } else {
+                type = (cType *)type->mParent;
+                if (type != 0) {
+                    goto loop_cast;
+                }
+                goto invalid_cast;
+            }
+        } else {
+invalid_cast:
+            isValid = 0;
+        }
+
+cast_done:
+        if (isValid != 0) {
+            other = (const gcDesiredEntityTemplate *)src;
+        }
+    }
+    self->operator=(*other);
+}
+
+int gcDesiredEntityTemplate::HasCategory(
+    const cHandlePairT<gcEnumeration, cSubHandleT<gcEnumerationEntry> > &category) const {
+    gcEntityTemplate *entityTemplate = (gcEntityTemplate *)Get(1);
+    if (entityTemplate != 0) {
+        return entityTemplate->HasCategory(category);
+    }
+    return 0;
 }
