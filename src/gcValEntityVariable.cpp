f0f79b01f795b9d5dba8f4eea04769c3c90bb842
autodecomp <autodecomp@localhost>
2026-04-29 19:48:18 -0500

    Match 2 functions (session 82390f34)
      - gcValEntityVariable::GetType
      - gcValEntityVariable::New

--- src/gcValEntityVariable.cpp
+++ src/gcValEntityVariable.cpp
@@ -51,8 +51,21 @@ public:
     static cBase *New(cMemPool *, cBase *);
 };
 
+struct PoolBlock {
+    char pad[0x1C];
+    char *allocTable;
+};
+
+struct AllocEntry {
+    short offset;
+    short pad;
+    void *(*fn)(void *, int, int, int, int);
+};
+
 extern const char gcValEntityVariable_base_name[];
 extern const char gcValEntityVariable_base_desc[];
+void gcDesiredObject_ctor(void *, void *);
+void gcDesiredEntityHelper_ctor(void *, int, int, int);
 
 void gcValEntityVariable::Write(cFile &file) const {
     cWriteBlock wb(file, 2);
@@ -100,4 +113,60 @@ const cType *gcValEntityVariable::GetType(void) const {
             0, 0, 0xD2, type_lvalue, gcValEntityVariable::New, 0, 0, 0);
     }
     return type_gcValEntityVariable;
+}
+
+cBase *gcValEntityVariable::New(cMemPool *pool, cBase *parent) {
+    void *block = ((void **)pool)[9];
+    char *allocTable = ((PoolBlock *)block)->allocTable;
+    AllocEntry *entry = (AllocEntry *)(allocTable + 0x28);
+    short off = entry->offset;
+    void *base = (char *)block + off;
+    gcValEntityVariable *result = 0;
+    gcValEntityVariable *obj =
+        (gcValEntityVariable *)entry->fn(base, 0x4C, 4, 0, 0);
+    if (obj != 0) {
+        ((void **)obj)[1] = (void *)0x37E6A8;
+        {
+            register void *type0 asm("a0");
+            __asm__ volatile("lui %0,0x1" : "=r"(type0));
+            ((cBase **)obj)[0] = parent;
+            __asm__ volatile("addiu %0,%0,-0x7ff8" : "+r"(type0));
+            ((void **)obj)[1] = type0;
+        }
+        char *sub = (char *)obj + 8;
+        gcDesiredObject_ctor(sub, obj);
+        {
+            register void *type1 asm("a0");
+            __asm__ volatile("lui %0,0x0\n\taddiu %0,%0,0x338"
+                             : "=r"(type1));
+            ((void **)obj)[3] = type1;
+        }
+        gcDesiredEntityHelper_ctor((char *)obj + 0x14, 1, 0, 0);
+        {
+            register void *helper_desc asm("a0");
+            __asm__ volatile("lui %0,0x39\n\taddiu %0,%0,-0x75b8"
+                             : "=r"(helper_desc));
+            ((void **)obj)[8] = (void *)0x37E6A8;
+            ((void **)obj)[3] = helper_desc;
+        }
+        ((char **)obj)[7] = sub;
+        ((void **)obj)[8] = (void *)0x388568;
+        ((char *)obj)[0x24] = 1;
+        ((char *)obj)[0x25] = 0;
+        ((int *)obj)[10] = 0;
+        ((int *)obj)[11] = 0;
+        int sub_or_1 = (int)sub | 1;
+        ((void **)obj)[14] = (void *)0x37E6A8;
+        ((int *)obj)[12] = sub_or_1;
+        ((gcValEntityVariable **)obj)[13] = obj;
+        ((void **)obj)[14] = (void *)0x388568;
+        ((char *)obj)[0x3C] = 1;
+        ((char *)obj)[0x3D] = 0;
+        ((int *)obj)[16] = 0;
+        int obj_or_1 = (int)obj | 1;
+        ((int *)obj)[17] = 0;
+        ((int *)obj)[18] = obj_or_1;
+        result = obj;
+    }
+    return (cBase *)result;
 }
