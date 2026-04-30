ce2828974988c0dd0510466e724666ed20889b6d
autodecomp <autodecomp@localhost>
2026-04-29 19:25:15 -0500

    Match 2 functions (session bbfa19f6)
      - gcUIEditBox::GetType
      - gcUIGeom::GetType

--- src/gcUIEditBox.cpp
+++ src/gcUIEditBox.cpp
@@ -1,6 +1,19 @@
 #include "gcUIEditBox.h"
 #include "gcGameCore.h"
 
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *,
+                                 cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
+};
+
+class gcUIControl {
+public:
+    static cBase *New(cMemPool *, cBase *);
+};
+
 inline void *operator new(unsigned int, void *p) {
     return p;
 }
@@ -12,6 +25,12 @@ struct gcUIEditBoxAllocRec {
 };
 
 extern char gcUIEditBoxvirtualtable[];
+extern cType *D_000385DC;
+extern cType *D_000385E0;
+extern cType *D_0009990C;
+extern cType *D_0009F40C;
+extern cType *D_0009F410;
+extern cType *D_0009F588;
 
 gcUIEditBox::~gcUIEditBox() {
     *(void **)((char *)this + 4) = gcUIEditBoxvirtualtable;
@@ -30,6 +49,36 @@ cBase *gcUIEditBox::New(cMemPool *pool, cBase *parent) {
         result = obj;
     }
     return (cBase *)result;
+}
+
+const cType *gcUIEditBox::GetType(void) const {
+    if (D_0009F588 == 0) {
+        if (D_0009F410 == 0) {
+            if (D_0009F40C == 0) {
+                if (D_0009990C == 0) {
+                    if (D_000385E0 == 0) {
+                        if (D_000385DC == 0) {
+                            D_000385DC = cType::InitializeType(
+                                (const char *)0x36D894, (const char *)0x36D89C,
+                                1, 0, 0, 0, 0, 0);
+                        }
+                        D_000385E0 = cType::InitializeType(
+                            0, 0, 2, D_000385DC,
+                            (cBase *(*)(cMemPool *, cBase *))0x1C3C58, 0, 0, 0);
+                    }
+                    D_0009990C = cType::InitializeType(
+                        0, 0, 0x84, D_000385E0, 0, 0, 0, 0);
+                }
+                D_0009F40C = cType::InitializeType(
+                    0, 0, 0x201, D_0009990C, gcUIControl::New, 0, 0, 0);
+            }
+            D_0009F410 = cType::InitializeType(
+                0, 0, 0x200, D_0009F40C, gcUITextControl::New, 0, 0, 0);
+        }
+        D_0009F588 = cType::InitializeType(
+            0, 0, 0x173, D_0009F410, gcUIEditBox::New, 0, 0, 0);
+    }
+    return D_0009F588;
 }
 
 int gcUIEditBox::GetCursor(cHandleT<eSprite> *handle) const {
