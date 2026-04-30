0a3cee879cd0eeaf34c9d103d14eee1cc35ca127
autodecomp <autodecomp@localhost>
2026-04-29 19:56:17 -0500

    Match 2 functions (session 38a827b1)
      - gcValHeadset::GetType
      - gcValKeyboardState::GetType

--- src/gcValHeadset.cpp
+++ src/gcValHeadset.cpp
@@ -11,10 +11,19 @@
 
 class cBase;
 class cFile;
+class cType;
 
 class cMemPool {
 public:
     static cMemPool *GetPoolFromPtr(const void *);
+};
+
+class cType {
+public:
+    static cType *InitializeType(const char *, const char *, unsigned int,
+                                 const cType *,
+                                 cBase *(*)(cMemPool *, cBase *),
+                                 const char *, const char *, unsigned int);
 };
 
 void cStrAppend(char *, const char *, ...);
@@ -82,6 +91,7 @@ public:
     int mField8;          // 0x8
     gcValHeadset(cBase *parent);
     ~gcValHeadset();
+    const cType *GetType(void) const;
     void GetText(char *) const;
     void Write(cFile &) const;
     int Read(cFile &, cMemPool *);
@@ -146,6 +156,37 @@ cBase *gcValHeadset::New(cMemPool *pool, cBase *parent) {
         result = obj;
     }
     return (cBase *)result;
+}
+
+static cType *type_base;
+static cType *type_expression;
+static cType *type_value;
+static cType *type_variable;
+static cType *type_gcValHeadset;
+
+const cType *gcValHeadset::GetType(void) const {
+    if (!type_gcValHeadset) {
+        if (!type_variable) {
+            if (!type_value) {
+                if (!type_expression) {
+                    if (!type_base) {
+                        type_base = cType::InitializeType((const char *)0x36D894,
+                                                          (const char *)0x36D89C,
+                                                          1, 0, 0, 0, 0, 0);
+                    }
+                    type_expression = cType::InitializeType(
+                        0, 0, 0x6A, type_base, 0, 0, 0, 0);
+                }
+                type_value = cType::InitializeType(
+                    0, 0, 0x6C, type_expression, 0, 0, 0, 0x80);
+            }
+            type_variable = cType::InitializeType(
+                0, 0, 0x6D, type_value, 0, 0, 0, 0);
+        }
+        type_gcValHeadset = cType::InitializeType(
+            0, 0, 0x1FE, type_variable, gcValHeadset::New, 0, 0, 0);
+    }
+    return type_gcValHeadset;
 }
 
 // ─────────────────────────────────────────────────────────────────────────
