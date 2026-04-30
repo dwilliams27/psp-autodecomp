afeb94920fd0d5088bdeec79dea6e1423f036912
autodecomp <autodecomp@localhost>
2026-04-30 01:44:01 -0500

    Match 2 functions (session 6c6f0ece)
      - eAnimation::AssignCopy
      - eCamera::operator=

--- src/eAnimation.cpp
+++ src/eAnimation.cpp
@@ -6,7 +6,21 @@ class cMemPool;
 class cObject {
 public:
     cObject(cBase *);
+    cObject &operator=(const cObject &);
 };
+
+template <class T> T *dcast(const cBase *);
+
+template <class T>
+class cArrayBase {
+public:
+    void *mData;
+    cArrayBase<T> &operator=(const cArrayBase<T> &);
+};
+
+struct eAnimationQuat;
+struct eAnimationVec3;
+struct eAnimationMorphFrame;
 
 class cType {
 public:
@@ -23,6 +37,7 @@ public:
 class eAnimation : public cObject {
 public:
     eAnimation(cBase *);
+    void AssignCopy(const cBase *);
     const cType *GetType(void) const;
     static cBase *New(cMemPool *, cBase *);
 };
@@ -75,6 +90,51 @@ cBase *eAnimation::New(cMemPool *pool, cBase *parent) {
         result = obj;
     }
     return (cBase *)result;
+}
+
+void eAnimation::AssignCopy(const cBase *base) {
+    eAnimation *other = dcast<eAnimation>(base);
+    ((cObject *)this)->operator=(*(const cObject *)other);
+    int *src44 = (int *)((char *)other + 0x44);
+    int value44 = *src44;
+    int *dst44 = (int *)((char *)this + 0x44);
+    int *src48 = (int *)((char *)other + 0x48);
+    int *dst48 = (int *)((char *)this + 0x48);
+    *dst44 = value44;
+    int value48 = *src48;
+    *dst48 = value48;
+    *(short *)((char *)this + 0x4C) = *(const short *)((char *)other + 0x4C);
+    *(short *)((char *)this + 0x4E) = *(const short *)((char *)other + 0x4E);
+    *(unsigned char *)((char *)this + 0x50) =
+        *(const unsigned char *)((char *)other + 0x50);
+    *(unsigned char *)((char *)this + 0x51) =
+        *(const unsigned char *)((char *)other + 0x51);
+    *(unsigned char *)((char *)this + 0x52) =
+        *(const unsigned char *)((char *)other + 0x52);
+    *(unsigned char *)((char *)this + 0x53) =
+        *(const unsigned char *)((char *)other + 0x53);
+    *(float *)((char *)this + 0x54) = *(const float *)((char *)other + 0x54);
+    *(float *)((char *)this + 0x58) = *(const float *)((char *)other + 0x58);
+    ((cArrayBase<unsigned char> *)((char *)this + 0x5C))
+        ->operator=(*(const cArrayBase<unsigned char> *)((char *)other + 0x5C));
+    ((cArrayBase<eAnimationQuat> *)((char *)this + 0x60))
+        ->operator=(*(const cArrayBase<eAnimationQuat> *)((char *)other + 0x60));
+    ((cArrayBase<eAnimationVec3> *)((char *)this + 0x64))
+        ->operator=(*(const cArrayBase<eAnimationVec3> *)((char *)other + 0x64));
+    ((cArrayBase<eAnimationVec3> *)((char *)this + 0x68))
+        ->operator=(*(const cArrayBase<eAnimationVec3> *)((char *)other + 0x68));
+    ((cArrayBase<unsigned short> *)((char *)this + 0x6C))
+        ->operator=(*(const cArrayBase<unsigned short> *)((char *)other + 0x6C));
+    ((cArrayBase<unsigned short> *)((char *)this + 0x70))
+        ->operator=(*(const cArrayBase<unsigned short> *)((char *)other + 0x70));
+    ((cArrayBase<unsigned short> *)((char *)this + 0x74))
+        ->operator=(*(const cArrayBase<unsigned short> *)((char *)other + 0x74));
+    ((cArrayBase<eAnimationMorphFrame> *)((char *)this + 0x78))
+        ->operator=(*(const cArrayBase<eAnimationMorphFrame> *)((char *)other + 0x78));
+    ((cArrayBase<unsigned char> *)((char *)this + 0x7C))
+        ->operator=(*(const cArrayBase<unsigned char> *)((char *)other + 0x7C));
+    ((cArrayBase<short> *)((char *)this + 0x80))
+        ->operator=(*(const cArrayBase<short> *)((char *)other + 0x80));
 }
 
 const cType *eAnimation::GetType(void) const {
--- src/eCamera.cpp
+++ src/eCamera.cpp
@@ -11,6 +11,8 @@ struct eViewport {
     int x, y, w, h;
 };
 
+typedef int v4sf_t __attribute__((mode(V4SF)));
+
 class eCamera;
 
 class cTimeValue {
@@ -34,6 +36,7 @@ public:
     void GetAdjustedFOV(float fov, float *out1, float *out2, float aspect) const;
     void AdjustFrustum(void);
     void Reset(void);
+    eCamera &operator=(const eCamera &);
 
     static void GetMaxWidthHeight(int *w, int *h);
 };
@@ -94,4 +97,135 @@ eCamera::eCamera(void) {
         ".word 0xfa090110\n"  /* sv.q C210, 0x110(s0) */
     );
     Reset();
+}
+
+eCamera &eCamera::operator=(const eCamera &other) {
+    int word0 = *(const int *)((const char *)&other + 0x00);
+    const int *srcWords = (const int *)((const char *)&other + 0x04);
+    int *dstWords = (int *)((char *)this + 0x04);
+    *(int *)((char *)this + 0x00) = word0;
+    int word1 = srcWords[0];
+    int word2 = srcWords[1];
+    int word3 = srcWords[2];
+    dstWords[0] = word1;
+    int word4 = srcWords[3];
+    dstWords[1] = word2;
+    dstWords[2] = word3;
+    dstWords[3] = word4;
+
+    unsigned int i = 0;
+    char *dst = (char *)this + 0x20;
+    const char *src = (const char *)&other + 0x20;
+    do {
+        float w = ((const mPlane *)src)->w;
+        *(v4sf_t *)dst = *(const v4sf_t *)src;
+        i++;
+        ((mPlane *)dst)->w = w;
+        dst += 0x10;
+        src += 0x10;
+    } while (i < 6);
+
+    i = 0;
+    dst = (char *)this + 0x80;
+    src = (const char *)&other + 0x80;
+    do {
+        float w = ((const mPlane *)src)->w;
+        *(v4sf_t *)dst = *(const v4sf_t *)src;
+        i++;
+        ((mPlane *)dst)->w = w;
+        dst += 0x10;
+        src += 0x10;
+    } while (i < 6);
+
+    *(v4sf_t *)((char *)this + 0x110) =
+        *(const v4sf_t *)((const char *)&other + 0x110);
+    i = 0;
+    *(v4sf_t *)((char *)this + 0x0E0) =
+        *(const v4sf_t *)((const char *)&other + 0x0E0);
+    dst = (char *)this + 0x120;
+    *(v4sf_t *)((char *)this + 0x0F0) =
+        *(const v4sf_t *)((const char *)&other + 0x0F0);
+    src = (const char *)&other + 0x120;
+    *(v4sf_t *)((char *)this + 0x100) =
+        *(const v4sf_t *)((const char *)&other + 0x100);
+    do {
+        *(v4sf_t *)dst = *(const v4sf_t *)src;
+        i++;
+        dst += 0x10;
+        src += 0x10;
+    } while (i < 4);
+
+    i = 0;
+    dst = (char *)this + 0x160;
+    src = (const char *)&other + 0x160;
+    do {
+        *(v4sf_t *)dst = *(const v4sf_t *)src;
+        i++;
+        dst += 0x10;
+        src += 0x10;
+    } while (i < 4);
+
+    i = 0;
+    dst = (char *)this + 0x1A0;
+    src = (const char *)&other + 0x1A0;
+    do {
+        *(v4sf_t *)dst = *(const v4sf_t *)src;
+        i++;
+        __asm__ volatile("" ::: "memory");
+        dst += 0x10;
+        src += 0x10;
+    } while (i < 4);
+
+    const int *src1E8 = (const int *)((const char *)&other + 0x1E8);
+    float value1E0 = *(const float *)((const char *)&other + 0x1E0);
+    *(float *)((char *)this + 0x1E0) = value1E0;
+    float value1E4 = *(const float *)((const char *)&other + 0x1E4);
+    int *dst1E8 = (int *)((char *)this + 0x1E8);
+    *(float *)((char *)this + 0x1E4) = value1E4;
+    *dst1E8 = *src1E8;
+    *(float *)((char *)this + 0x1EC) =
+        *(const float *)((const char *)&other + 0x1EC);
+    *(float *)((char *)this + 0x1F0) =
+        *(const float *)((const char *)&other + 0x1F0);
+    *(float *)((char *)this + 0x1F4) =
+        *(const float *)((const char *)&other + 0x1F4);
+    __asm__ volatile("" ::: "memory");
+    *(v4sf_t *)((char *)this + 0x200) =
+        *(const v4sf_t *)((const char *)&other + 0x200);
+    *(v4sf_t *)((char *)this + 0x210) =
+        *(const v4sf_t *)((const char *)&other + 0x210);
+    *(v4sf_t *)((char *)this + 0x220) =
+        *(const v4sf_t *)((const char *)&other + 0x220);
+    float value230 = *(const float *)((const char *)&other + 0x230);
+    *(float *)((char *)this + 0x230) = value230;
+    const char *src250 = (const char *)&other;
+    int value234 = *(const int *)((const char *)&other + 0x234);
+    src250 += 0x250;
+    *(int *)((char *)this + 0x234) = value234;
+    *(v4sf_t *)((char *)this + 0x240) =
+        *(const v4sf_t *)((const char *)&other + 0x240);
+    int *dst250 = (int *)((char *)this + 0x250);
+    int value250 = *(const int *)src250;
+    *dst250 = value250;
+    int value254 = *(const int *)((const char *)&other + 0x254);
+    *(int *)((char *)this + 0x254) = value254;
+    int value258 = *(const int *)((const char *)&other + 0x258);
+    char *dstBase = (char *)this + 0x25C;
+    const char *srcBase = (const char *)&other + 0x25C;
+    *(int *)((char *)this + 0x258) = value258;
+
+    int offset = 0;
+    int count = 0x16;
+    do {
+        const int *srcPair = (const int *)((int)srcBase + offset);
+        int pair0 = srcPair[0];
+        int pair1 = srcPair[1];
+        int *dstPair = (int *)(dstBase + offset);
+        dstPair[0] = pair0;
+        dstPair[1] = pair1;
+        count--;
+        offset += 8;
+    } while (count != 0);
+
+    return *this;
 }
