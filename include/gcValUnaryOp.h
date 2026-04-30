466bdf28eeecbb8d4ccc4baf7ba43e265e894277
autodecomp <autodecomp@localhost>
2026-04-29 21:41:33 -0500

    Match 2 functions (session ba7220ed)
      - gcValUIVisible::GetType
      - gcValUnaryOp::GetType

--- include/gcValUnaryOp.h
+++ include/gcValUnaryOp.h
@@ -3,6 +3,7 @@
 
 class cBase;
 class cMemPool;
+class cType;
 class gcExpression;
 
 class gcValUnaryOp {
@@ -12,6 +13,7 @@ public:
     gcExpression *GetChild(int) const;
     int GetExprFlags(void) const;
     void GetText(char *) const;
+    const cType *GetType(void) const;
     static cBase *New(cMemPool *, cBase *);
 };
 
