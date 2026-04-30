5ae736dd6d7a467253bc44c60877ad430fdecb4f
Farmerbilly27 <dmw2796@gmail.com>
2026-04-29 23:13:08 -0500

    Clean interrupted overnight run

--- src/gcGeomCurveConfig.cpp
+++ src/gcGeomCurveConfig.cpp
@@ -10,15 +10,40 @@ public:
                                  const char *, const char *, unsigned int);
 };
 
-class gcGeomCurveConfig {
-public:
-    static cBase *New(cMemPool *, cBase *);
-    const cType *GetType(void) const;
-};
-
 class gcEntityGeomConfig {
 public:
+    char _base[0x50];
+    bool mBool50;
+    char _pad51[0xF];
+
+    gcEntityGeomConfig(cBase *);
     static cBase *New(cMemPool *, cBase *);
+};
+
+class cHandle {
+public:
+    int mIndex;
+};
+
+void gcEvent_gcEvent(void *, cBase *, const char *);
+
+class gcGeomCurveConfig : public gcEntityGeomConfig {
+public:
+    bool mBool60;
+    char _pad61[3];
+    int mInt64;
+    char mEvent68[0x1C];
+    int mTaggedBase84;
+    unsigned int mValue88;
+    bool mBool8C;
+    char _pad8D[3];
+    cHandle mHandle90;
+    unsigned int mValue94;
+    void *mHelpers98;
+
+    gcGeomCurveConfig(cBase *);
+    static cBase *New(cMemPool *, cBase *);
+    const cType *GetType(void) const;
 };
 
 class gcGeomTrailConfig {
@@ -44,6 +69,22 @@ extern cType *D_0009F5E0;
 extern cType *D_0009F648;
 extern cType *D_0009F784;
 extern cType *D_0009F78C;
+
+// gcGeomCurveConfig::gcGeomCurveConfig(cBase *) @ 0x001546f8
+gcGeomCurveConfig::gcGeomCurveConfig(cBase *parent)
+    : gcEntityGeomConfig(parent) {
+    *(void **)((char *)this + 4) = (void *)0x38C9E8;
+    mBool60 = 0;
+    mInt64 = 1;
+    gcEvent_gcEvent((char *)this + 0x68, (cBase *)this, (const char *)0x36E5D4);
+    mTaggedBase84 = (int)((unsigned int)this | 1);
+    mValue88 = (unsigned int)-1;
+    mBool8C = 0;
+    mHandle90.mIndex = 0;
+    mValue94 = 0;
+    mHelpers98 = 0;
+    mBool50 = 1;
+}
 
 // gcGeomCurveConfig::GetType(void) const @ 0x0031bd84
 const cType *gcGeomCurveConfig::GetType(void) const {
