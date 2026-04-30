f32e963226ec7a517ad285bae7d961ad09b7759a
autodecomp <autodecomp@localhost>
2026-04-30 00:13:15 -0500

    Match 2 functions (session 4975f6ef)
      - gcValObjectHasRelation::Write
      - gcValObjectHasRelation::AssignCopy

--- src/gcValObjectHasRelation.cpp
+++ src/gcValObjectHasRelation.cpp
@@ -1,13 +1,49 @@
 class cBase;
+class cFile;
 class cMemPool;
 class cType;
 
 class cType {
 public:
+    char pad[0x1C];
+    cType *mParent;
+
     static cType *InitializeType(const char *, const char *, unsigned int,
                                  const cType *,
                                  cBase *(*)(cMemPool *, cBase *),
                                  const char *, const char *, unsigned int);
+};
+
+class cWriteBlock {
+public:
+    int _data[2];
+
+    cWriteBlock(cFile &, unsigned int);
+    void Write(bool);
+    void WriteBase(const cBase *);
+    void End(void);
+};
+
+class gcValue {
+public:
+    cBase *mParent;
+    void *mClassDesc;
+
+    void Write(cFile &) const;
+};
+
+class gcDesiredEnumerationEntry {
+public:
+    cBase *mParent;
+    void *mClassDesc;
+    unsigned char mField8;
+    unsigned char mField9;
+    char padA[2];
+    int mFieldC;
+    int mField10;
+    int mBase14;
+
+    void Write(cWriteBlock &) const;
 };
 
 extern char cBaseclassdesc[];
@@ -25,10 +61,29 @@ struct AllocEntry {
     void *(*fn)(void *, int, int, int, int);
 };
 
-class gcValObjectHasRelation {
+struct DispatchEntry {
+    short offset;
+    short pad;
+    cType *(*fn)(void *);
+};
+
+struct WriteEntry {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
+};
+
+class gcValObjectHasRelation : public gcValue {
 public:
+    int mBase8;
+    gcDesiredEnumerationEntry mDesiredC;
+    bool mField24;
+
+    gcValObjectHasRelation &operator=(const gcValObjectHasRelation &);
+    void AssignCopy(const cBase *);
     const cType *GetType(void) const;
     static cBase *New(cMemPool *, cBase *);
+    void Write(cFile &) const;
 };
 
 static cType *type_base asm("D_000385DC");
@@ -86,4 +141,82 @@ const cType *gcValObjectHasRelation::GetType(void) const {
                                   gcValObjectHasRelation::New, 0, 0, 0);
     }
     return type_gcValObjectHasRelation;
+}
+
+void gcValObjectHasRelation::AssignCopy(const cBase *base) {
+    const gcValObjectHasRelation *other = 0;
+
+    if (base != 0) {
+        if (!type_gcValObjectHasRelation) {
+            if (!type_value) {
+                if (!type_expression) {
+                    if (!type_base) {
+                        type_base = cType::InitializeType(
+                            (const char *)0x36D894, (const char *)0x36D89C,
+                            1, 0, 0, 0, 0, 0);
+                    }
+                    type_expression = cType::InitializeType(
+                        0, 0, 0x6A, type_base, 0, 0, 0, 0);
+                }
+                type_value = cType::InitializeType(
+                    0, 0, 0x6C, type_expression, 0, 0, 0, 0x80);
+            }
+            type_gcValObjectHasRelation = cType::InitializeType(
+                0, 0, 0x140, type_value, gcValObjectHasRelation::New,
+                0, 0, 0);
+        }
+
+        DispatchEntry *entry =
+            (DispatchEntry *)((char *)*(void **)((char *)base + 4) + 8);
+        cType *wanted = type_gcValObjectHasRelation;
+        cType *type = entry->fn((char *)base + entry->offset);
+        int ok;
+
+        if (wanted == 0) {
+            ok = 0;
+        } else if (type != 0) {
+        loop:
+            if (type == wanted) {
+                ok = 1;
+            } else {
+                type = type->mParent;
+                if (type != 0) {
+                    goto loop;
+                }
+                goto fail;
+            }
+        } else {
+fail:
+            ok = 0;
+        }
+        if (ok != 0) {
+            other = (const gcValObjectHasRelation *)base;
+        }
+    }
+
+    operator=(*other);
+}
+
+void gcValObjectHasRelation::Write(cFile &file) const {
+    cWriteBlock wb(file, 3);
+    ((const gcValue *)this)->Write(file);
+    int val = this->mBase8;
+    int flag = 0;
+    if (val & 1) {
+        flag = 1;
+    }
+    cBase *ptr;
+    if (flag != 0) {
+        ptr = 0;
+    } else {
+        ptr = (cBase *)val;
+    }
+    wb.WriteBase(ptr);
+    char *desc = *(char **)((char *)this + 0x10);
+    WriteEntry *entry = (WriteEntry *)(desc + 0x28);
+    short off = entry->offset;
+    char *sub = (char *)this + 0xC;
+    entry->fn(sub + off, (void *)wb._data[0]);
+    wb.Write(mField24);
+    wb.End();
 }
