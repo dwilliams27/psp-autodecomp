121d65d46fc19e2406cf1aef86cdfb7dbe715edf
autodecomp <autodecomp@localhost>
2026-04-30 21:40:26 -0500

    Match 1 functions (session 475ca360)
      - nwSocket::SendConnect

--- src/nwSocket.cpp
+++ src/nwSocket.cpp
@@ -2,6 +2,8 @@
 // connection array.
 
 class nwAddress;
+class cOutStream;
+class nwOutPacket;
 
 class nwSocketHandle {
 public:
@@ -38,7 +40,33 @@ struct ConnLite {
 
 }  // namespace
 
-class nwTransport;
+class nwTransport {
+public:
+    bool Send(const nwAddress &, const nwOutPacket &);
+};
+
+class cOutStream {
+public:
+    unsigned char *mData;
+    int mCapacity;
+    int mBitPos;
+    int mCRC;
+    unsigned char mDirty;
+    char mPad11;
+    unsigned char mOverflow;
+    char _pad13;
+    int mCRCBitPos;
+
+    void Write(int, int, bool);
+    void Write(unsigned int, int, bool);
+};
+
+class nwOutPacket {
+public:
+    int mUnk0;
+    int mUnk4;
+    int mSizeBits;
+};
 
 class nwConnection {
 public:
@@ -75,6 +103,8 @@ public:
     void Destroy(void);
     void AddressFromString(const char *, nwAddress *) const;
     nwConnectionHandle GetConnection(int) const;
+    void SendConnect(const nwAddress &, const char *);
+    void WriteId(cOutStream &);
     void ReceiveReject(nwInPacket &, nwConnectionHandle);
     void Update(cTimeValue);
     static void UpdateAll(cTimeValue);
@@ -83,6 +113,7 @@ public:
 
 extern "C" {
     void cStrCopy(char *, const char *);
+    int cStrLength(const char *);
     void nwSocket___dtor_nwSocket_void(nwSocket *, int);
     extern nwSocket *D_00034958[];
 }
@@ -130,6 +161,55 @@ nwSocket::nwSocket(nwTransport *transport, nwSocketHandle handle,
       mField2C(0)
 {
     cStrCopy(mName, "");
+}
+
+// ------------------------------------------------------------------
+void nwSocket::SendConnect(const nwAddress &addr, const char *name) {
+    struct {
+        cOutStream out;
+        unsigned char data[0x4B0];
+        nwTransport *transport;
+    } pkt;
+
+    nwTransport *transport = mTransport;
+    int zero = 0;
+    pkt.out.mData = pkt.data;
+    pkt.out.mCapacity = 0x4B0;
+    pkt.out.mBitPos = 0;
+    pkt.out.mCRC = 0;
+    pkt.out.mDirty = zero;
+    pkt.out.mPad11 = 0;
+    pkt.out.mOverflow = 0;
+    pkt.out.mCRCBitPos = 0;
+    pkt.transport = transport;
+
+    TransportObj *t = (TransportObj *)transport;
+    VtEntry *ent = (VtEntry *)((char *)t->vtable + 0x30);
+    typedef void (*ConnectFn)(void *, cOutStream &);
+    ((ConnectFn)ent->fn)((char *)t + ent->adjust, pkt.out);
+
+    pkt.out.Write((unsigned int)0, 4, false);
+    WriteId(pkt.out);
+
+    int length = cStrLength(name) & 0xFFFF;
+    {
+        unsigned int lenArg = (unsigned int)(length & 0xFFFF);
+        bool sign2 = true;
+        pkt.out.Write(lenArg, 0x10, sign2);
+    }
+
+    int i = 0;
+    if (i < length) {
+        do {
+            bool sign = false;
+            signed char v = name[i];
+            int iv = v;
+            pkt.out.Write((signed char)iv, 8, sign);
+            i += 1;
+        } while (i < length);
+    }
+
+    mTransport->Send(addr, *(const nwOutPacket *)&pkt.out);
 }
 
 // ------------------------------------------------------------------
