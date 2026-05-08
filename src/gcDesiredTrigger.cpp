a7071463ab92e65925588915093295b0fbd1f356
autodecomp <autodecomp@localhost>
2026-05-08 17:20:48 -0500

    Match 2 functions (session f7421215)
      - gcDesiredTrigger::Read
      - gcDesiredUIDialog::Read

--- src/gcDesiredTrigger.cpp
+++ src/gcDesiredTrigger.cpp
@@ -7,8 +7,14 @@
 
 class cBase;
 class cFile;
-class cMemPool;
+class cFileHandle;
+class cReadBlock;
 class cType;
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
 
 class cWriteBlock {
 public:
@@ -18,13 +24,20 @@ public:
     void End(void);
 };
 
+class cFileSystem {
+public:
+    static void Read(cFileHandle *, void *, unsigned int);
+};
+
 class cHandle {
 public:
+    void Read(cReadBlock &, cMemPool *);
     void Write(cWriteBlock &) const;
 };
 
 class gcDesiredObject {
 public:
+    int Read(cFile &, cMemPool *);
     void Write(cFile &) const;
 };
 
@@ -37,6 +50,9 @@ public:
 };
 
 void gcDesiredObject_gcDesiredObject(void *, cBase *);
+extern "C" void cFile_SetCurrentPos(void *, unsigned int);
+extern "C" void __0oKcReadBlockctR6FcFileUib(void *, cFile &, unsigned int, bool);
+extern "C" void __0oKcReadBlockdtv(void *, int);
 
 extern char gcDesiredObjectT_gcDesiredTrigger_gcDesiredTriggerHelper_gcTrigger_virtualtable[];
 extern char gcDesiredTriggervirtualtable[];
@@ -58,9 +74,15 @@ struct AllocEntry {
 
 class gcDesiredTrigger {
 public:
+    int Read(cFile &, cMemPool *);
     void Write(cFile &) const;
     static cBase *New(cMemPool *, cBase *);
     const cType *GetType(void) const;
+};
+
+class gcDesiredUIDialog {
+public:
+    int Read(cFile &, cMemPool *);
 };
 
 // 0x0012d6e8 - gcDesiredTrigger::Write(cFile &) const
@@ -73,6 +95,54 @@ void gcDesiredTrigger::Write(cFile &file) const {
     ((const cHandle *)((const char *)this + 16))->Write(wb2);
     wb2.End();
     wb.End();
+}
+
+// 0x0012d768 - gcDesiredTrigger::Read(cFile &, cMemPool *)
+int gcDesiredTrigger::Read(cFile &file, cMemPool *pool) {
+    int result = 1;
+    int rb[5];
+    int inner[5];
+    __0oKcReadBlockctR6FcFileUib(rb, file, 2, true);
+    if (rb[3] != 2 || ((gcDesiredObject *)this)->Read(file, pool) == 0) {
+        cFile_SetCurrentPos(*(void **)&rb[0], rb[1]);
+        __0oKcReadBlockdtv(rb, 2);
+        return 0;
+    }
+
+    __0oKcReadBlockctR6FcFileUib(inner, *(cFile *)rb[0], 3, true);
+    cFileSystem::Read(*(cFileHandle **)inner[0], (char *)this + 0x0C, 4);
+    *(int *)((char *)this + 0x10) = 0;
+    {
+        cHandle *handle = (cHandle *)((char *)this + 0x10);
+        handle->Read(*(cReadBlock *)inner, cMemPool::GetPoolFromPtr(handle));
+    }
+    __0oKcReadBlockdtv(inner, 2);
+    __0oKcReadBlockdtv(rb, 2);
+    return result;
+}
+
+// 0x0012deb4 - gcDesiredUIDialog::Read(cFile &, cMemPool *)
+int gcDesiredUIDialog::Read(cFile &file, cMemPool *pool) {
+    int result = 1;
+    int rb[5];
+    int inner[5];
+    __0oKcReadBlockctR6FcFileUib(rb, file, 1, true);
+    if (rb[3] != 1 || ((gcDesiredObject *)this)->Read(file, pool) == 0) {
+        cFile_SetCurrentPos(*(void **)&rb[0], rb[1]);
+        __0oKcReadBlockdtv(rb, 2);
+        return 0;
+    }
+
+    __0oKcReadBlockctR6FcFileUib(inner, *(cFile *)rb[0], 3, true);
+    cFileSystem::Read(*(cFileHandle **)inner[0], (char *)this + 0x0C, 4);
+    *(int *)((char *)this + 0x10) = 0;
+    {
+        cHandle *handle = (cHandle *)((char *)this + 0x10);
+        handle->Read(*(cReadBlock *)inner, cMemPool::GetPoolFromPtr(handle));
+    }
+    __0oKcReadBlockdtv(inner, 2);
+    __0oKcReadBlockdtv(rb, 2);
+    return result;
 }
 
 // 0x00278c28 - gcDesiredTrigger::New(cMemPool *, cBase *) static
