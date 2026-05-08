75904cfa0266462cc727cdda3e1d77788c50e83a
autodecomp <autodecomp@localhost>
2026-05-07 22:38:11 -0500

    Match 2 functions (session bb696c78)
      - gcSimpleController::Read
      - eSpriteMtlSet::Read

--- src/gcSimpleController.cpp
+++ src/gcSimpleController.cpp
@@ -94,6 +94,15 @@ public:
     void End(void);
 };
 
+class cReadBlock {
+public:
+    int _data[5];
+    cReadBlock(cFile &, unsigned int, bool);
+    ~cReadBlock(void);
+};
+
+void cFile_SetCurrentPos(void *, unsigned int);
+
 extern "C" void gcEntityController__gcEntityController_cBaseptr(void *, cBase *);
 
 class gcEntityController {
@@ -102,6 +111,7 @@ public:
     gcEntityController(cBase *);
     ~gcEntityController();
     void Write(cFile &) const;
+    int Read(cFile &, cMemPool *);
 };
 
 class gcSimpleController : public gcEntityController {
@@ -111,6 +121,7 @@ public:
     void AssignCopy(const cBase *);
     const cType *GetType(void) const;
     void Write(cFile &) const;
+    int Read(cFile &, cMemPool *);
     static gcSimpleController *New(cMemPool *, cBase *);
     static void operator delete(void *p) {
         cMemPool *pool = cMemPool::GetPoolFromPtr(p);
@@ -133,6 +144,18 @@ void gcSimpleController::Write(cFile &file) const {
     cWriteBlock wb(file, 1);
     gcEntityController::Write(file);
     wb.End();
+}
+
+// ── gcSimpleController::Read(cFile &, cMemPool *) @ 0x00157FB0 ──
+int gcSimpleController::Read(cFile &file, cMemPool *pool) {
+    int result;
+    cReadBlock rb(file, 1, true);
+    __asm__("ori %0, $0, 1" : "=r"(result));
+    if ((unsigned int)rb._data[3] == 1 && gcEntityController::Read(file, pool)) goto success;
+    cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+    return 0;
+success:
+    return result;
 }
 
 // ── gcSimpleController::~gcSimpleController(void) @ 0x0032032c ──
