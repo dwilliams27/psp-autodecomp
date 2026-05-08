c7e66f3da85f6363535448dbf981f9ef86524671
autodecomp <autodecomp@localhost>
2026-05-08 11:18:18 -0500

    Match 2 functions (session eb538870)
      - eParticleSystemMtl::Read
      - cListSubscriber::Read

--- src/cListSubscriber.cpp
+++ src/cListSubscriber.cpp
@@ -10,6 +10,9 @@ extern "C" void free(void *);
 
 void cFile_SetCurrentPos(void *, unsigned int);
 
+extern "C" void __0oKcReadBlockctR6FcFileUib(void *, cFile &, unsigned int, bool);
+extern "C" void __0oKcReadBlockdtv(void *, int);
+
 class cReadBlock {
 public:
     int _data[5];
@@ -166,13 +169,15 @@ void cListSubscriber::SetNumVisible(int numVisible) {
 }
 
 int cListSubscriber::Read(cFile &file, cMemPool *) {
-    int result;
-    __asm__ volatile("ori %0, $0, 1" : "=r"(result));
-    cReadBlock rb(file, 1, true);
-    if ((unsigned int)rb._data[3] != 1) {
-        cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+    int result = 1;
+    int rb[5];
+    __0oKcReadBlockctR6FcFileUib(rb, file, 1, true);
+    if (rb[3] != 1) {
+        cFile_SetCurrentPos(*(void **)&rb[0], rb[1]);
+        __0oKcReadBlockdtv(rb, 2);
         return 0;
     }
+    __0oKcReadBlockdtv(rb, 2);
     return result;
 }
 
