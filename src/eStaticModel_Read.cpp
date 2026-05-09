33fb41712a787b185ca9379ef883da993f4c2a20
autodecomp <autodecomp@localhost>
2026-05-08 19:09:34 -0500

    Match 1 functions (session d49eeec5)
      - eStaticModel::Read

--- src/eStaticModel_Read.cpp
+++ src/eStaticModel_Read.cpp
@@ -31,48 +31,51 @@ public:
 };
 
 void cFile_SetCurrentPos(void *, unsigned int);
+extern "C" void __0oKcReadBlockctR6FcFileUib(void *, cFile &, unsigned int, bool);
+extern "C" void __0oKcReadBlockdtv(void *, int);
 
-#pragma control sched=1
+#pragma control sched=2
 
 // -- eStaticModel::Read(cFile &, cMemPool *) @ 0x00041b94 --
 int eStaticModel::Read(cFile &file, cMemPool *pool) {
-    int result;
-    __asm__ volatile("ori %0, $0, 1" : "=r"(result));
-    __asm__ volatile("" : : "r"(this), "r"(pool), "r"(&file));
-    cReadBlock rb(file, 2, true);
-    unsigned int version = (unsigned int)rb._data[3];
+    int result = 1;
+    int rb[5];
+    __0oKcReadBlockctR6FcFileUib(rb, file, 2, true);
+    unsigned int version = (unsigned int)rb[3];
     if (version >= 3 || version < 1) goto fail;
     if (!((eStaticGeom *)this)->Read(file, pool)) goto fail;
 
     {
-        void *h = *(void **)rb._data[0];
+        void *h = *(void **)rb[0];
         cFileSystem::Read(h, (char *)this + 0x40, 0xC);
     }
     {
-        void *h = *(void **)rb._data[0];
+        void *h = *(void **)rb[0];
         cFileSystem::Read(h, (char *)this + 0x10, 0xC);
     }
     {
-        void *h = *(void **)rb._data[0];
+        void *h = *(void **)rb[0];
         cFileSystem::Read(h, (char *)this + 0x20, 0xC);
     }
     {
-        void *h = *(void **)rb._data[0];
+        void *h = *(void **)rb[0];
         cFileSystem::Read(h, (char *)this + 0x30, 0xC);
     }
-    if ((unsigned int)rb._data[3] >= 2) goto read_field98;
+    if ((unsigned int)rb[3] >= 2) goto read_field98;
     goto platform;
 
 fail:
-    cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+    cFile_SetCurrentPos(*(void **)&rb[0], rb[1]);
+    __0oKcReadBlockdtv(rb, 2);
     return 0;
 
 read_field98:
     {
-        void *h = *(void **)rb._data[0];
+        void *h = *(void **)rb[0];
         cFileSystem::Read(h, (char *)this + 0x98, 4);
     }
 platform:
     PlatformRead(file, pool);
+    __0oKcReadBlockdtv(rb, 2);
     return result;
 }
