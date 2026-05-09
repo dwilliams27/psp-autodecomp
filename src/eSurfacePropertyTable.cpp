7f23cfc54796aecdecb6f4c1ee70dbc7ecb1a0ed
autodecomp <autodecomp@localhost>
2026-05-08 23:50:56 -0500

    Match 2 functions (session 26800e88)
      - eSurfacePropertyTable::Read
      - gcValNavMeshDirection::Read

--- src/eSurfacePropertyTable.cpp
+++ src/eSurfacePropertyTable.cpp
@@ -17,6 +17,26 @@ public:
 };
 
 class cFile;
+class cFileHandle;
+class cMemPool;
+
+class cFileSystem {
+public:
+    static void Read(void *, void *, unsigned int);
+};
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
+
+class cReadBlock {
+public:
+    int _data[5];
+    cReadBlock(cFile &, unsigned int, bool);
+    ~cReadBlock(void);
+    void ReadBase(cMemPool *, cBase *, cBase *&);
+};
 
 class cWriteBlock {
 public:
@@ -36,6 +56,7 @@ public:
 
     cBaseArray &operator=(const cBaseArray &);
     void Write(cWriteBlock &) const;
+    void Read(cReadBlock &);
 };
 
 template <class T>
@@ -44,6 +65,13 @@ public:
     void *mData;
 
     cArrayBase &operator=(const cArrayBase &);
+    bool SetSize(int, const cArrayBase &);
+};
+
+template <class T>
+class cArray : public cArrayBase<T> {
+public:
+    void Read(cReadBlock &);
 };
 
 class eSurfacePropertyPair;
@@ -52,14 +80,54 @@ class eSurfacePropertyTable : public cObject {
 public:
     void AssignCopy(const cBase *);
     const cType *GetType(void) const;
+    int Read(cFile &, cMemPool *);
     void Write(cFile &) const;
     static cBase *New(cMemPool *, cBase *);
+};
+
+class cName {
+public:
+    void Read(cReadBlock &);
+};
+
+class gcValue {
+public:
+    int Read(cFile &, cMemPool *);
+};
+
+class gcValNavMeshDirection : public gcValue {
+public:
+    int Read(cFile &, cMemPool *);
+};
+
+struct ReadEntry {
+    short offset;
+    short pad;
+    void (*fn)(void *, cFile *, cMemPool *);
+};
+
+struct PoolBlock {
+    char pad[0x1C];
+    char *allocTable;
+};
+
+struct DeleteEntry {
+    short offset;
+    short pad;
+    void (*fn)(void *, void *);
 };
 
 extern cType *D_000385DC;
 extern cType *D_000385E0;
 extern cType *D_000385E4;
 extern cType *D_00046A20;
+
+extern "C" void cFile_SetCurrentPos(void *, unsigned int);
+extern "C" int __0fHcObjectEReadR6FcFileP6IcMemPool(cObject *, cFile &,
+                                                    cMemPool *);
+extern "C" void __0oKcReadBlockctR6FcFileUib(void *, cFile &, unsigned int,
+                                             bool);
+extern "C" void __0oKcReadBlockdtv(void *, int);
 
 #pragma control sched=1
 void eSurfacePropertyTable::AssignCopy(const cBase *base) {
@@ -170,4 +238,183 @@ void eSurfacePropertyTable::Write(cFile &file) const {
     }
 
     wb.End();
+}
+
+int eSurfacePropertyTable::Read(cFile &file, cMemPool *pool) {
+    register int result __asm__("$18");
+    __asm__ volatile("ori %0, $0, 1" : "=r"(result));
+    cReadBlock rb(file, 2, true);
+
+    unsigned int version = rb._data[3];
+    if (version >= 3) goto fail;
+    if (version < 1) goto fail;
+    if (__0fHcObjectEReadR6FcFileP6IcMemPool((cObject *)this, file, pool))
+        goto success;
+
+fail:
+    cFile_SetCurrentPos(*(void **)&rb._data[0], rb._data[1]);
+    return 0;
+
+success:
+    ((cBaseArray *)((char *)this + 0x44))->Read(rb);
+
+    cArrayBase<cArray<eSurfacePropertyPair> > old;
+    int count;
+    {
+        void *h = *(void **)rb._data[0];
+        __asm__ volatile("" : "+r"(h));
+        cFileSystem::Read(h, &count, 4);
+    }
+
+    register cArrayBase<cArray<eSurfacePropertyPair> > *tables __asm__("$4") =
+        (cArrayBase<cArray<eSurfacePropertyPair> > *)((char *)this + 0x4C);
+    register int size __asm__("$5") = count;
+    __asm__ volatile("" : "+r"(tables), "+r"(size));
+    ((volatile cArrayBase<cArray<eSurfacePropertyPair> > *)&old)->mData = 0;
+    ((volatile cArrayBase<cArray<eSurfacePropertyPair> > *)&old)->mData = 0;
+    tables->SetSize(size, old);
+
+    void *data = old.mData;
+    int oldCount = 0;
+    if (data != 0) {
+        oldCount = ((int *)data)[-1] & 0x3FFFFFFF;
+    }
+
+    int index = 0;
+    int inRange = index < oldCount;
+    if (inRange != 0) {
+        do {
+            index += 1;
+            inRange = index < oldCount;
+        } while (inRange != 0);
+    }
+
+    if (data != 0) {
+        data = (char *)data - 4;
+        if (data != 0) {
+            cMemPool *oldPool = cMemPool::GetPoolFromPtr(data);
+            char *block = ((char **)oldPool)[9];
+            char *allocTable = ((PoolBlock *)block)->allocTable;
+            register DeleteEntry *rec __asm__("$5") =
+                (DeleteEntry *)(allocTable + 0x30);
+            __asm__ volatile("" : "+r"(rec));
+            short off = rec->offset;
+            void *target = block + off;
+            void (*fn)(void *, void *) = rec->fn;
+            fn(target, data);
+        }
+        old.mData = 0;
+    }
+
+    int total = count;
+    int row = 0;
+    cArray<eSurfacePropertyPair> *pairs =
+        *(cArray<eSurfacePropertyPair> **)((char *)this + 0x4C);
+    if (row < total) {
+        register int offset __asm__("$4");
+        __asm__ volatile("ori %0, $0, 0" : "=r"(offset));
+        pairs = (cArray<eSurfacePropertyPair> *)((char *)pairs + offset);
+        do {
+            pairs->Read(rb);
+            row += 1;
+            pairs = (cArray<eSurfacePropertyPair> *)((char *)pairs + 4);
+        } while (row < total);
+    }
+
+    {
+        char *typeInfo = *(char **)((char *)this + 4);
+        register ReadEntry *rec __asm__("$5") = (ReadEntry *)(typeInfo + 0x38);
+        __asm__ volatile("" : "+r"(rec));
+        short off = rec->offset;
+        void *target = (char *)this + off;
+        void (*fn)(void *, cFile *, cMemPool *) = rec->fn;
+        fn(target, (cFile *)pool, 0);
+    }
+
+    return result;
+}
+
+#pragma control sched=2
+int gcValNavMeshDirection::Read(cFile &file, cMemPool *pool) {
+    int result = 1;
+    int rb[5];
+
+    __0oKcReadBlockctR6FcFileUib(rb, file, 0xC, true);
+    if ((unsigned int)rb[3] >= 0xD || (unsigned int)rb[3] < 0xA ||
+        gcValue::Read(file, pool) == 0) {
+        cFile_SetCurrentPos(*(void **)&rb[0], rb[1]);
+        __0oKcReadBlockdtv(rb, 2);
+        return 0;
+    }
+
+    {
+        char *typeInfo = *(char **)((char *)this + 0x0C);
+        ReadEntry *rec = (ReadEntry *)(typeInfo + 0x30);
+        char *desired = (char *)this + 0x08;
+        short off = rec->offset;
+        char *target = desired + off;
+        register int f __asm__("$18") = rb[0];
+        cMemPool *desiredPool = cMemPool::GetPoolFromPtr(desired);
+        rec->fn(target, (cFile *)f, desiredPool);
+    }
+
+    int sp14;
+    int value = *(int *)((char *)this + 0x34);
+    int tag = value & 1;
+    int flag = 0;
+    if (tag != 0) {
+        flag = 1;
+    }
+
+    int outValue;
+    if (flag != 0) {
+        outValue = 0;
+        goto out_done;
+    }
+    outValue = value;
+out_done:
+    sp14 = outValue;
+
+    int flag2 = 0;
+    if (tag != 0) {
+        flag2 = 1;
+    }
+
+    int base;
+    if (flag2 != 0) {
+        base = value & ~1;
+    } else {
+        base = *(int *)value;
+    }
+
+    cMemPool *childPool = cMemPool::GetPoolFromPtr((char *)this + 0x34);
+    ((cReadBlock *)rb)->ReadBase(childPool, (cBase *)base, *(cBase **)&sp14);
+
+    int newValue;
+    if (sp14 == 0) {
+        newValue = base | 1;
+    } else {
+        newValue = sp14;
+    }
+    int readFile = rb[0];
+    *(int *)((char *)this + 0x34) = newValue;
+
+    cFileSystem::Read(*(void **)readFile, (char *)this + 0x38, 4);
+    cFileSystem::Read(*(void **)rb[0], (char *)this + 0x3C, 4);
+    ((cName *)((char *)this + 0x40))->Read(*(cReadBlock *)rb);
+    cFileSystem::Read(*(void **)rb[0], (char *)this + 0x58, 4);
+
+    int version = rb[3];
+    int versionUnder11 = (unsigned int)version < 0xB;
+    int versionUnder12 = (unsigned int)version < 0xC;
+    if (versionUnder11 == 0) {
+        cFileSystem::Read(*(void **)rb[0], (char *)this + 0x5C, 4);
+        versionUnder12 = (unsigned int)rb[3] < 0xC;
+    }
+    if (versionUnder12 == 0) {
+        cFileSystem::Read(*(void **)rb[0], (char *)this + 0x60, 4);
+    }
+
+    __0oKcReadBlockdtv(rb, 2);
+    return result;
 }
