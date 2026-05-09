ecabe14ec6f9d7c8b1a2ca21184c6daf56fe19dd
autodecomp <autodecomp@localhost>
2026-05-08 22:52:37 -0500

    Match 2 functions (session cbd4646f)
      - gcValObjectHasRelation::Read
      - gcDoSetObjectRelationship::Read

--- src/gcValObjectHasRelation.cpp
+++ src/gcValObjectHasRelation.cpp
@@ -1,7 +1,24 @@
 class cBase;
 class cFile;
+class cFileHandle;
 class cMemPool;
+class cReadBlock;
 class cType;
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
+
+class cFile {
+public:
+    void SetCurrentPos(unsigned int);
+};
+
+class cFileSystem {
+public:
+    static void Read(cFileHandle *, void *, unsigned int);
+};
 
 class cType {
 public:
@@ -29,7 +46,27 @@ public:
     cBase *mParent;
     void *mClassDesc;
 
+    int Read(cFile &, cMemPool *);
     void Write(cFile &) const;
+};
+
+class gcExpression {
+};
+
+class gcAction : public gcExpression {
+public:
+    cBase *mOwner;
+    void *mClassDesc;
+    unsigned int mNext;
+
+    int Read(cFile &, cMemPool *);
+};
+
+class cReadBlock {
+public:
+    int _data[5];
+
+    void ReadBase(cMemPool *, cBase *, cBase *&);
 };
 
 class gcDesiredEnumerationEntry {
@@ -73,6 +110,12 @@ struct WriteEntry {
     void (*fn)(void *, void *);
 };
 
+struct ReadEntry {
+    short offset;
+    short pad;
+    void (*fn)(void *, cFile *, cMemPool *);
+};
+
 class gcValObjectHasRelation : public gcValue {
 public:
     int mBase8;
@@ -83,13 +126,26 @@ public:
     void AssignCopy(const cBase *);
     const cType *GetType(void) const;
     static cBase *New(cMemPool *, cBase *);
+    int Read(cFile &, cMemPool *);
     void Write(cFile &) const;
+};
+
+class gcDoSetObjectRelationship : public gcAction {
+public:
+    int mFieldC;
+    int mField10;
+
+    int Read(cFile &, cMemPool *);
 };
 
 static cType *type_base asm("D_000385DC");
 static cType *type_expression asm("D_000385D8");
 static cType *type_value asm("D_0009F3E8");
 static cType *type_gcValObjectHasRelation asm("D_0009F8C0");
+
+extern "C" void __0oKcReadBlockctR6FcFileUib(void *, cFile &, unsigned int,
+                                             bool);
+extern "C" void __0oKcReadBlockdtv(void *, int);
 
 // ── gcValObjectHasRelation::New(cMemPool *, cBase *) static @ 0x00355768 ──
 cBase *gcValObjectHasRelation::New(cMemPool *pool, cBase *parent) {
@@ -219,4 +275,168 @@ void gcValObjectHasRelation::Write(cFile &file) const {
     entry->fn(sub + off, (void *)wb._data[0]);
     wb.Write(mField24);
     wb.End();
+}
+
+int gcValObjectHasRelation::Read(cFile &file, cMemPool *pool) {
+    int result = 1;
+    int rb[5];
+
+    __0oKcReadBlockctR6FcFileUib(rb, file, 3, true);
+    if (rb[3] != 3 || gcValue::Read(file, pool) == 0) {
+        ((cFile *)rb[0])->SetCurrentPos(rb[1]);
+        __0oKcReadBlockdtv(rb, 2);
+        return 0;
+    }
+
+    int sp14;
+    int value = *(int *)((char *)this + 0x08);
+    int tag = value & 1;
+    int flag = 0;
+    if (tag != 0) {
+        flag = 1;
+    }
+
+    int outValue;
+    if (flag != 0) {
+        outValue = 0;
+        goto out_done;
+    }
+    outValue = value;
+out_done:
+    sp14 = outValue;
+
+    int flag2 = 0;
+    if (tag != 0) {
+        flag2 = 1;
+    }
+
+    int base;
+    if (flag2 != 0) {
+        base = value & ~1;
+    } else {
+        base = *(int *)value;
+    }
+
+    cMemPool *childPool = cMemPool::GetPoolFromPtr((char *)this + 0x08);
+    ((cReadBlock *)rb)->ReadBase(childPool, (cBase *)base, *(cBase **)&sp14);
+
+    int newValue;
+    if (sp14 == 0) {
+        newValue = base | 1;
+    } else {
+        newValue = sp14;
+    }
+    *(int *)((char *)this + 0x08) = newValue;
+
+    char *typeInfo = *(char **)((char *)this + 0x10);
+    ReadEntry *rec = (ReadEntry *)(typeInfo + 0x30);
+    char *desired = (char *)this + 0x0C;
+    short off = rec->offset;
+    char *target = desired + off;
+    cFile *f = *(cFile **)&rb[0];
+    cMemPool *desiredPool = cMemPool::GetPoolFromPtr(desired);
+    rec->fn(target, f, desiredPool);
+
+    char sp18;
+    cFileSystem::Read(*(cFileHandle **)rb[0], &sp18, 1);
+    mField24 = sp18 != 0;
+
+    __0oKcReadBlockdtv(rb, 2);
+    return result;
+}
+
+int gcDoSetObjectRelationship::Read(cFile &file, cMemPool *pool) {
+    int result = 1;
+    int rb[5];
+
+    __0oKcReadBlockctR6FcFileUib(rb, file, 4, true);
+    if (rb[3] != 4 || gcAction::Read(file, pool) == 0) {
+        ((cFile *)rb[0])->SetCurrentPos(rb[1]);
+        __0oKcReadBlockdtv(rb, 2);
+        return 0;
+    }
+
+    int sp14;
+    int value = *(int *)((char *)this + 0x0C);
+    int tag = value & 1;
+    int flag = 0;
+    if (tag != 0) {
+        flag = 1;
+    }
+
+    int outValue;
+    if (flag != 0) {
+        outValue = 0;
+        goto first_out_done;
+    }
+    outValue = value;
+first_out_done:
+    sp14 = outValue;
+
+    int flag2 = 0;
+    if (tag != 0) {
+        flag2 = 1;
+    }
+
+    int base;
+    if (flag2 != 0) {
+        base = value & ~1;
+    } else {
+        base = *(int *)value;
+    }
+
+    cMemPool *childPool = cMemPool::GetPoolFromPtr((char *)this + 0x0C);
+    ((cReadBlock *)rb)->ReadBase(childPool, (cBase *)base, *(cBase **)&sp14);
+
+    int value2 = *(int *)((char *)this + 0x10);
+    int tag2 = value2 & 1;
+    int newValue;
+    if (sp14 == 0) {
+        newValue = base | 1;
+    } else {
+        newValue = sp14;
+    }
+    *(int *)((char *)this + 0x0C) = newValue;
+
+    int sp18;
+    int flag3 = 0;
+    if (tag2 != 0) {
+        flag3 = 1;
+    }
+
+    int outValue2;
+    if (flag3 != 0) {
+        outValue2 = 0;
+        goto second_out_done;
+    }
+    outValue2 = value2;
+second_out_done:
+    sp18 = outValue2;
+
+    int flag4 = 0;
+    if (tag2 != 0) {
+        flag4 = 1;
+    }
+
+    int base2;
+    if (flag4 != 0) {
+        base2 = value2 & ~1;
+    } else {
+        base2 = *(int *)value2;
+    }
+
+    cMemPool *childPool2 = cMemPool::GetPoolFromPtr((char *)this + 0x10);
+    ((cReadBlock *)rb)->ReadBase(childPool2, (cBase *)base2,
+                                 *(cBase **)&sp18);
+
+    int newValue2;
+    if (sp18 == 0) {
+        newValue2 = base2 | 1;
+    } else {
+        newValue2 = sp18;
+    }
+    *(int *)((char *)this + 0x10) = newValue2;
+
+    __0oKcReadBlockdtv(rb, 2);
+    return result;
 }
