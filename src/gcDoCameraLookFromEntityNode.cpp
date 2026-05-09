1ff670b4dbc96cacd9a791ef293c311b8ee43173
autodecomp <autodecomp@localhost>
2026-05-08 23:15:28 -0500

    Match 2 functions (session cfcd93d0)
      - gcDoCameraLookFromEntityNode::Read
      - gcDoLobbyScoreboardOp::Read

--- src/gcDoCameraLookFromEntityNode.cpp
+++ src/gcDoCameraLookFromEntityNode.cpp
@@ -1,5 +1,18 @@
 #include "cBase.h"
 
+class cFileHandle;
+class cReadBlock;
+
+class cMemPool {
+public:
+    static cMemPool *GetPoolFromPtr(const void *);
+};
+
+class cFileSystem {
+public:
+    static void Read(cFileHandle *, void *, unsigned int);
+};
+
 class cType {
 public:
     static cType *InitializeType(const char *, const char *, unsigned int,
@@ -20,12 +33,19 @@ public:
 
 class cName {
 public:
+    void Read(cReadBlock &);
     void Write(cWriteBlock &) const;
 };
 
 class gcDesiredValue {
 public:
+    void Read(cReadBlock &);
     void Write(cWriteBlock &) const;
+};
+
+class cReadBlock {
+public:
+    void ReadBase(cMemPool *, cBase *, cBase *&);
 };
 
 struct WriteRec {
@@ -75,8 +95,24 @@ public:
     void AssignCopy(const cBase *);
     gcDoCameraLookFromEntityNode &operator=(const gcDoCameraLookFromEntityNode &);
     const cType *GetType(void) const;
+    int Read(cFile &, cMemPool *);
     void Write(cFile &) const;
 };
+
+class gcDoLobbyScoreboardOp {
+public:
+    int Read(cFile &, cMemPool *);
+};
+
+struct ReadRec {
+    short offset;
+    short _pad;
+    void (*fn)(void *, cFile *, cMemPool *);
+};
+
+extern "C" void cFile_SetCurrentPos(void *, unsigned int);
+extern "C" void __0oKcReadBlockctR6FcFileUib(void *, cFile &, unsigned int, bool);
+extern "C" void __0oKcReadBlockdtv(void *, int);
 
 static cType *type_base;
 static cType *type_expression;
@@ -244,4 +280,193 @@ void gcDoCameraLookFromEntityNode::Write(cFile &file) const {
     wb.Write(*(const bool *)((const char *)this + 0x94));
     ((const gcDesiredValue *)((const char *)this + 0x90))->Write(wb);
     wb.End();
+}
+
+int gcDoCameraLookFromEntityNode::Read(cFile &file, cMemPool *pool) {
+    int result = 1;
+    int rb[5];
+    char outerFlag;
+    int inner[5];
+    char innerFlag;
+    int ok;
+
+    __0oKcReadBlockctR6FcFileUib(rb, file, 3, true);
+    if ((unsigned int)rb[3] >= 4 || (unsigned int)rb[3] < 1) {
+        goto fail_outer;
+    }
+
+    __0oKcReadBlockctR6FcFileUib(inner, file, 1, true);
+    if (inner[3] != 1) {
+        goto fail_inner;
+    }
+    if (((gcAction *)this)->Read(file, pool) != 0) {
+        goto read_inner;
+    }
+
+fail_inner:
+    cFile_SetCurrentPos(*(void **)&inner[0], inner[1]);
+    __0oKcReadBlockdtv(inner, 2);
+    ok = 0;
+    goto after_inner;
+
+read_inner:
+    {
+        char *typeInfo0 = *(char **)((char *)this + 0x10);
+        char *base0 = (char *)this + 0x0C;
+        ReadRec *rec0 = (ReadRec *)(typeInfo0 + 0x30);
+        short off0 = rec0->offset;
+        cFile *f0 = *(cFile **)&inner[0];
+        rec0->fn(base0 + off0, f0, cMemPool::GetPoolFromPtr(base0));
+    }
+
+    ((gcDesiredValue *)((char *)this + 0x44))->Read(*(cReadBlock *)inner);
+
+    cFileSystem::Read(*(cFileHandle **)inner[0], &innerFlag, 1);
+    *(unsigned char *)((char *)this + 0x48) = innerFlag != 0;
+
+    __0oKcReadBlockdtv(inner, 2);
+    ok = 1;
+
+after_inner:
+    if (ok != 0) {
+        goto read_outer;
+    }
+
+fail_outer:
+    cFile_SetCurrentPos(*(void **)&rb[0], rb[1]);
+    __0oKcReadBlockdtv(rb, 2);
+    return 0;
+
+read_outer:
+    {
+        char *typeInfo1 = *(char **)((char *)this + 0x50);
+        char *base1 = (char *)this + 0x4C;
+        ReadRec *rec1 = (ReadRec *)(typeInfo1 + 0x30);
+        short off1 = rec1->offset;
+        cFile *f1 = *(cFile **)&rb[0];
+        rec1->fn(base1 + off1, f1, cMemPool::GetPoolFromPtr(base1));
+    }
+
+    ((cName *)((char *)this + 0x78))->Read(*(cReadBlock *)rb);
+    int version = rb[3];
+    if ((unsigned int)version >= 2) {
+        cFileSystem::Read(*(cFileHandle **)rb[0], &outerFlag, 1);
+        int outerTmp = outerFlag;
+        __asm__ volatile("" ::: "memory");
+        version = rb[3];
+        *(unsigned char *)((char *)this + 0x94) = outerTmp != 0;
+    }
+    if ((unsigned int)version >= 3) {
+        ((gcDesiredValue *)((char *)this + 0x90))->Read(*(cReadBlock *)rb);
+    }
+
+    __0oKcReadBlockdtv(rb, 2);
+    return result;
+}
+
+int gcDoLobbyScoreboardOp::Read(cFile &file, cMemPool *pool) {
+    int result = 1;
+    int rb[5];
+
+    __0oKcReadBlockctR6FcFileUib(rb, file, 2, true);
+    if ((unsigned int)rb[3] >= 3 || (unsigned int)rb[3] < 1 ||
+        ((gcAction *)this)->Read(file, pool) == 0) {
+        cFile_SetCurrentPos(*(void **)&rb[0], rb[1]);
+        __0oKcReadBlockdtv(rb, 2);
+        return 0;
+    }
+
+    cFileSystem::Read(*(cFileHandle **)rb[0], (char *)this + 0x0C, 4);
+    cFileSystem::Read(*(cFileHandle **)rb[0], (char *)this + 0x10, 4);
+    ((gcDesiredValue *)((char *)this + 0x14))->Read(*(cReadBlock *)rb);
+    ((gcDesiredValue *)((char *)this + 0x18))->Read(*(cReadBlock *)rb);
+
+    int sp14;
+    int value0 = *(int *)((char *)this + 0x1C);
+    int tag0 = value0 & 1;
+    int flag0 = 0;
+    if (tag0 != 0) {
+        flag0 = 1;
+    }
+
+    int out0;
+    if (flag0 != 0) {
+        out0 = 0;
+        goto out_done0;
+    }
+    out0 = value0;
+out_done0:
+    sp14 = out0;
+
+    int flag0b = 0;
+    if (tag0 != 0) {
+        flag0b = 1;
+    }
+
+    int base0;
+    if (flag0b != 0) {
+        base0 = value0 & ~1;
+    } else {
+        base0 = *(int *)value0;
+    }
+
+    ((cReadBlock *)rb)->ReadBase(cMemPool::GetPoolFromPtr((char *)this + 0x1C),
+                                 (cBase *)base0, *(cBase **)&sp14);
+
+    int sp18;
+    int value1 = *(int *)((char *)this + 0x20);
+    int tag1 = value1 & 1;
+    int new0;
+    if (sp14 == 0) {
+        new0 = base0 | 1;
+    } else {
+        new0 = sp14;
+    }
+    *(int *)((char *)this + 0x1C) = new0;
+
+    int flag1 = 0;
+    if (tag1 != 0) {
+        flag1 = 1;
+    }
+
+    int out1;
+    if (flag1 != 0) {
+        out1 = 0;
+        goto out_done1;
+    }
+    out1 = value1;
+out_done1:
+    sp18 = out1;
+
+    int flag1b = 0;
+    if (tag1 != 0) {
+        flag1b = 1;
+    }
+
+    int base1;
+    if (flag1b != 0) {
+        base1 = value1 & ~1;
+    } else {
+        base1 = *(int *)value1;
+    }
+
+    ((cReadBlock *)rb)->ReadBase(cMemPool::GetPoolFromPtr((char *)this + 0x20),
+                                 (cBase *)base1, *(cBase **)&sp18);
+
+    int new1;
+    if (sp18 != 0) {
+        new1 = sp18;
+    } else {
+        new1 = base1 | 1;
+    }
+    int version = rb[3];
+    *(int *)((char *)this + 0x20) = new1;
+
+    if ((unsigned int)version >= 2) {
+        ((gcDesiredValue *)((char *)this + 0x24))->Read(*(cReadBlock *)rb);
+        ((gcDesiredValue *)((char *)this + 0x28))->Read(*(cReadBlock *)rb);
+    }
+
+    __0oKcReadBlockdtv(rb, 2);
+    return result;
 }
